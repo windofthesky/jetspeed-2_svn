dcb6feb052b05ee9cc2ab4bce13ed0c11d5c6faf
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-23 13:18:29 +0000

    added additional components
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@185760 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/webapp/WEB-INF/assembly/jetspeed.groovy
+++ portal/src/webapp/WEB-INF/assembly/jetspeed.groovy
@@ -1,11 +1,40 @@
 import org.picocontainer.defaults.DefaultPicoContainer
+import org.picocontainer.defaults.ConstantParameter
+import org.picocontainer.ComponentAdapter
+import org.picocontainer.defaults.ConstructorComponentAdapter
+import org.picocontainer.Parameter
+import org.picocontainer.defaults.ConstantParameter
+import org.picocontainer.defaults.ComponentParameter
 import org.apache.jetspeed.locator.JetspeedTemplateLocator
 import org.apache.jetspeed.components.ComponentAssemblyTestCase
 import org.apache.jetspeed.idgenerator.JetspeedIdGenerator
 import org.apache.jetspeed.page.impl.CastorXmlPageManager
 import org.apache.jetspeed.Jetspeed
+import org.apache.jetspeed.components.hsql.HSQLServerComponent
+import org.apache.jetspeed.components.persistence.store.ojb.OJBTypeIntializer
+import org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl
+import org.apache.jetspeed.components.persistence.store.impl.DefaultPersistenceStoreContainer
+import org.apache.jetspeed.components.persistence.store.PersistenceStoreContainer
+import org.apache.jetspeed.components.util.system.FSSystemResourceUtilImpl
+
+import org.apache.jetspeed.components.portletregsitry.PortletRegistryComponentImpl
+import org.apache.jetspeed.components.portletregsitry.PortletRegistryComponent
+
+
+// WARNING!!!!!!
+// DO NOT use {Class}.class as it appears to be broken in Groovy
+// You end getting a Class instance of the type java.lang.Class
+// instead of the requested type!  This causes AssignabilityExceptions
+// in pico.  You need to use Class.forName() instead.
+
+
 
 applicationRoot = Jetspeed.getRealPath("/")
+
+//
+// Resource Location Utility
+//
+FSSystemResourceUtilImpl resourceUtil = new FSSystemResourceUtilImpl(applicationRoot)
 
 // create the root container
 container = new DefaultPicoContainer()
@@ -33,5 +62,38 @@ mapping = applicationRoot + "/WEB-INF/conf/page-mapping.xml"
 // TODO: modelclasses, extension, scanrate, cachesize
 container.registerComponentInstance("CastorXmlPageManager", 
                                      new CastorXmlPageManager(idgenerator, mapping, root))
+
+//
+// HSQL Server (no datasource support)
+//                
+
+container.registerComponentInstance(new HSQLServerComponent(9001, "sa","",applicationRoot+"WEB-INF/db/hsql/Registry",false, true))                     
+
+//
+// Persistence component
+//
+
+PersistenceContainer pContainer = new DefaultPersistenceStoreContainer(15000, 10000)
+
+OJBTypeIntializer ojbBootstrap = new OJBTypeIntializer(resourceUtil, "WEB-INF/conf/ojb", "OJB.properties", null)
+
+pContainer.registerComponentInstance(ojbBootstrap)
+
+Class OTMStoreClass = Class.forName("org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl")
+ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", OTMStoreClass, new Parameter[] {new ConstantParameter("jetspeed")})
+
+pContainer.registerComponent(ca)
+
+Class pContainerClass = Class.forName("org.apache.jetspeed.components.persistence.store.PersistenceStoreContainer")
+
+container.registerComponentInstance(pContainerClass, pContainer);
+
+//
+// Portlet Registry
+//
+
+Class registryClass = Class.forName("org.apache.jetspeed.components.portletregsitry.PortletRegistryComponent")
+Parameter[] regParams = new Parameter[] {new ComponentParameter(pContainerClass), new ConstantParameter("jetspeed")}
+container.registerComponentInstance(registryClass, new PortletRegistryComponentImpl() );
 
 return container
