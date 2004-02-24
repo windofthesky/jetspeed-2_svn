038ffc28737ff18618a0a3cd74bc1e7582652fec
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-24 00:39:25 +0000

    - now checks to see where the db really should be (a maven req) - spelling correction
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@185779 13f79535-47bb-0310-9956-ffa450edef68

--- components/registry/src/java/org/apache/jetspeed/containers/registry.container.groovy
+++ components/registry/src/java/org/apache/jetspeed/containers/registry.container.groovy
@@ -6,7 +6,6 @@ import org.picocontainer.Parameter
 import org.picocontainer.defaults.ConstantParameter
 import org.picocontainer.defaults.ComponentParameter
 import org.hsqldb.jdbcDriver
-
 import org.apache.jetspeed.components.hsql.HSQLServerComponent
 import org.apache.jetspeed.components.hsql.HSQLServerComponent
 import org.apache.jetspeed.components.jndi.JNDIComponent
@@ -23,6 +22,7 @@ import org.apache.jetspeed.components.util.system.FSSystemResourceUtilImpl
 import org.apache.jetspeed.components.portletregsitry.PortletRegistryComponentImpl
 import org.apache.jetspeed.components.portletregsitry.PortletRegistryComponent
 
+import java.io.File
 
 // WARNING!!!!!!
 // DO NOT use {Class}.class as it appears to be broken in Groovy
@@ -32,46 +32,21 @@ import org.apache.jetspeed.components.portletregsitry.PortletRegistryComponent
 
 
 
-applicationRoot = Jetspeed.getRealPath("/")
-
-//
-// Resource Location Utility
-//
-FSSystemResourceUtilImpl resourceUtil = new FSSystemResourceUtilImpl(applicationRoot)
-
 // create the root container
 container = new DefaultPicoContainer()
 
-//
-// Template Locator component assembly
-//
-roots = [ applicationRoot + "/WEB-INF/templates" ]
-container.registerComponentInstance("TemplateLocator", new JetspeedTemplateLocator(roots))
 
-//
-// ID Generator
-//
-Long counterStart = 65536
-peidPrefix = "P-"
-peidSuffix = ""
-container.registerComponentInstance("IdGenerator", new JetspeedIdGenerator(counterStart, peidPrefix, peidSuffix))
 
-//
-// Page Manager
-//
-root = applicationRoot + "/WEB-INF/pages"
-// TODO: move this into a class loader resource
-mapping = applicationRoot + "/WEB-INF/conf/page-mapping.xml"
-// TODO: modelclasses, extension, scanrate, cachesize
-container.registerComponentInstance("CastorXmlPageManager", 
-                                     new CastorXmlPageManager(idgenerator, mapping, root))
 
-//
-// HSQL Server 
-//                
-
-container.registerComponentInstance(new HSQLServerComponent(9001, "sa","",applicationRoot+"WEB-INF/db/hsql/Registry",false, true))                     
-
+// This is the HSQL engine that holds the test registry
+if(new File("../../portal/test/db/hsql").exists())
+{
+   container.registerComponentInstance(new HSQLServerComponent(9001, "sa","","../../portal/test/db/hsql/Registry",false, true))
+}
+else
+{
+   container.registerComponentInstance(new HSQLServerComponent(9001, "sa","","./portal/test/db/hsql/Registry",false, true))
+}
 
 // This JNDI component helps us publish the datasource
 Class jndiClass = Class.forName("org.apache.jetspeed.components.jndi.JNDIComponent")
@@ -80,25 +55,20 @@ container.registerComponentImplementation(jndiClass, tyrexJndiClass)
 
 // Create a datasource based on the HSQL server we just created
 Class dsClass = Class.forName("org.apache.jetspeed.components.datasource.DatasourceComponent")
-container.registerComponentInstance(dsClass, new DBCPDatasourceComponent("sa","", "org.hsqldb.jdbcDriver", "jdbc:hsqldb:hsql://127.0.0.1", 5, 5000, GenericObjectPool.WHEN_EXHAUSTED_GROW, true))
-
+container.registerComponentInstance(dsClass, new DBCPDatasourceComponent("sa","", "org.hsqldb.jdbcDriver", "jdbc:hsqldb:hsql://127.0.0.1", 20, 5000, GenericObjectPool.WHEN_EXHAUSTED_GROW, true))
 
 //
-// Persistence component
-//
+// Persistence
+PersistenceContainer pContainer = new DefaultPersistenceStoreContainer(300000, 10000)
+Class pContainerClass = Class.forName("org.apache.jetspeed.components.persistence.store.PersistenceStoreContainer")
 
-PersistenceContainer pContainer = new DefaultPersistenceStoreContainer(15000, 10000)
+// Parameter[] storeParams = new Parameter[] {new ConstantParameter("jetspeed")}
 
-OJBTypeIntializer ojbBootstrap = new OJBTypeIntializer(resourceUtil, "WEB-INF/conf/ojb", "OJB.properties", null)
-
-pContainer.registerComponentInstance(ojbBootstrap)
 
 Class OTMStoreClass = Class.forName("org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl")
 ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", OTMStoreClass, new Parameter[] {new ConstantParameter("jetspeed")})
 
 pContainer.registerComponent(ca)
-
-Class pContainerClass = Class.forName("org.apache.jetspeed.components.persistence.store.PersistenceStoreContainer")
 
 container.registerComponentInstance(pContainerClass, pContainer);
 
