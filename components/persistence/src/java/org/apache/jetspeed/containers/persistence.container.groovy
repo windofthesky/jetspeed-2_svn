24d5cd5c44b57e71b4f5f3c65844ec43b73dbcc2
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-24 00:33:58 +0000

    removed type init'r since OJB meta data is now in the CP
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@185774 13f79535-47bb-0310-9956-ffa450edef68

--- components/persistence/src/java/org/apache/jetspeed/containers/persistence.container.groovy
+++ components/persistence/src/java/org/apache/jetspeed/containers/persistence.container.groovy
@@ -9,17 +9,7 @@ import org.apache.jetspeed.components.util.system.FSSystemResourceUtilImpl
 import org.picocontainer.ComponentAdapter
 import org.picocontainer.defaults.ConstructorComponentAdapter
 
-
 container = new DefaultPersistenceStoreContainer(15000, 10000)
-
-FSSystemResourceUtilImpl resourceUtil = new FSSystemResourceUtilImpl("../../portal/src/webapp")
-
-OJBTypeIntializer ojbBootstrap = new OJBTypeIntializer(resourceUtil, "WEB-INF/conf/ojb", "OJB.properties", null)
-
-container.registerComponentInstance(ojbBootstrap)
-
-// Parameter[] storeParams = new Parameter[] {new ConstantParameter("jetspeed")}
-
 
 Class OTMStoreClass = Class.forName("org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl")
 ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", OTMStoreClass, new Parameter[] {new ConstantParameter("jetspeed")})
