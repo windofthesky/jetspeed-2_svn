3209a9f2f0d6baa769af0c33c20ebcbba81835dd
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-04 21:23:58 +0000

    reverting to ojb-Rc4 and PB
    commented out autodeploy for now
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@185918 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/webapp/WEB-INF/assembly/jetspeed.groovy
+++ portal/src/webapp/WEB-INF/assembly/jetspeed.groovy
@@ -17,7 +17,8 @@ import org.apache.jetspeed.components.datasource.DBCPDatasourceComponent
 import org.apache.jetspeed.components.datasource.DatasourceComponent
 import org.apache.commons.pool.impl.GenericObjectPool
 import org.apache.jetspeed.components.persistence.store.ojb.OJBTypeIntializer
-import org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl
+// import org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl
+import org.apache.jetspeed.components.persistence.store.ojb.pb.PBStore
 import org.apache.jetspeed.components.persistence.store.impl.DefaultPersistenceStoreContainer
 import org.apache.jetspeed.components.persistence.store.PersistenceStoreContainer
 import org.apache.jetspeed.components.util.system.FSSystemResourceUtilImpl
@@ -106,8 +107,9 @@ PersistenceContainer pContainer = new DefaultPersistenceStoreContainer(15000, 10
 
 // pContainer.registerComponentInstance(ojbBootstrap)
 
-Class OTMStoreClass = Class.forName("org.apache.jetspeed.components.persistence.store.ojb.otm.OTMStoreImpl")
-ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", OTMStoreClass, new Parameter[] {new ConstantParameter("jetspeed")})
+
+// ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", OTMStoreClass, new Parameter[] {new ConstantParameter("jetspeed")})
+ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", PBStore, new Parameter[] {new ConstantParameter("jetspeed")})
 
 pContainer.registerComponent(ca)
 
