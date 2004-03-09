f043a06d8184018b62a00eba3f1c6fb131a116e8
dlestrat <dlestrat@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-09 15:58:32 +0000

    Remove System.setProperty. Now part of the provider configuration.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@185977 13f79535-47bb-0310-9956-ffa450edef68

--- components/prefs/src/test/org/apache/jetspeed/prefs/TestPreferences.java
+++ components/prefs/src/test/org/apache/jetspeed/prefs/TestPreferences.java
@@ -70,7 +70,6 @@ public class TestPreferences extends AbstractComponentAwareTestCase
     public void setUp() throws Exception
     {
         super.setUp();
-        System.setProperty("java.util.prefs.PreferencesFactory", "org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl");
         container = (MutablePicoContainer) getContainer();
         pms = (PropertyManager) container.getComponentInstance(PropertyManager.class);
     }
