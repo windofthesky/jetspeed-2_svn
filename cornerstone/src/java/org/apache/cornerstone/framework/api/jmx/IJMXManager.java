df0ae41ce7080e641f1b64a73ae59b158802e652
jyang <jyang@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-10 01:01:26 +0000

    added PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@185996 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cornerstone/src/java/org/apache/cornerstone/framework/api/jmx/IJMXManager.java
@@ -0,0 +1,8 @@
+package org.apache.cornerstone.framework.api.jmx;
+
+public interface IJMXManager
+{
+	public static final String REVISION = "$Revision$";
+
+	public void manage(Object managedObject);
+}
