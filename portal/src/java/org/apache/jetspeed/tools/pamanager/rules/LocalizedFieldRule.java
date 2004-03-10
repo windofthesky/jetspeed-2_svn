cf037f7d372faa7bc0faf1de07dad5a4c8283200
jford <jford@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-10 01:40:13 +0000

    Rule for digesting localized fields.  Need to finish impl.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186002 13f79535-47bb-0310-9956-ffa450edef68

---
+++ portal/src/java/org/apache/jetspeed/tools/pamanager/rules/LocalizedFieldRule.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Mar 9, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.jetspeed.tools.pamanager.rules;
+
+import org.apache.commons.digester.Rule;
+
+/**
+ * This class helps load internationalized fields
+ *
+ * @author <a href="mailto:jford@apache.org">Jeremy Ford</a>
+ * @version $Id$
+ */
+public class LocalizedFieldRule extends Rule
+{
+
+}
