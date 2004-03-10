23fdc6a23bb0416a61c23a34822d8da162c48d05
jford <jford@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-10 17:13:59 +0000

    Finished impl
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186026 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/java/org/apache/jetspeed/tools/pamanager/rules/LocalizedFieldRule.java
+++ portal/src/java/org/apache/jetspeed/tools/pamanager/rules/LocalizedFieldRule.java
@@ -6,7 +6,15 @@
  */
 package org.apache.jetspeed.tools.pamanager.rules;
 
+import java.util.Locale;
+
 import org.apache.commons.digester.Rule;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jetspeed.om.common.GenericMetadata;
+import org.apache.jetspeed.om.common.LocalizedField;
+import org.apache.jetspeed.om.common.impl.LocalizedFieldImpl;
+import org.xml.sax.Attributes;
 
 /**
  * This class helps load internationalized fields
@@ -16,5 +24,58 @@ import org.apache.commons.digester.Rule;
  */
 public class LocalizedFieldRule extends Rule
 {
+    protected final static Log log = LogFactory.getLog(LocalizedFieldRule.class);
+    /**
+     * Handle the beginning of an XML element.
+     *
+     * @param attributes The attributes of this element
+     *
+     * @exception Exception if a processing error occurs
+     */
+    public void begin(String namespace, String name, Attributes attributes)
+    throws Exception {
 
+        if (digester.getLogger().isDebugEnabled())
+            digester.getLogger().debug("Setting localized field " + name);
+        
+        LocalizedField child = new LocalizedFieldImpl();
+
+        if(name.equals("metadata"))
+        {
+            String nameAttr = attributes.getValue("name");
+            child.setName(nameAttr);
+        }
+        else
+        {
+            child.setName(name);
+        }
+        String language = attributes.getValue("xml:lang");
+        Locale locale = null;
+        if(language == null)
+        {
+            locale = new Locale("en");
+        }
+        else
+        {
+            locale = new Locale(language);
+        }
+
+        child.setLocale(locale);
+        digester.push(child);
+    }
+
+    public void body(String namespace, String name, String text)
+    throws Exception
+    {
+        LocalizedField child = (LocalizedField) digester.peek(0);
+        child.setValue(text);
+    }
+
+    public void end(String namespace, String name)
+    throws Exception
+    {
+        LocalizedField child = (LocalizedField) digester.pop();
+        GenericMetadata metadata = (GenericMetadata)digester.peek();
+        metadata.addField(child);
+    }
 }
