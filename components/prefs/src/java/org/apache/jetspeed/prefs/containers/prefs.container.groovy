2bc0aa9a01ddba03db11eb88a4015da3b41b88ab
dlestrat <dlestrat@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-11 02:56:36 +0000

    Finally got a satisfying way to assemble containers and instantiate PreferencesProvider during assembly. Refactoring prefs container.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186034 13f79535-47bb-0310-9956-ffa450edef68

--- components/prefs/src/java/org/apache/jetspeed/prefs/containers/prefs.container.groovy
+++ components/prefs/src/java/org/apache/jetspeed/prefs/containers/prefs.container.groovy
@@ -23,30 +23,17 @@ import java.io.File
  * and use it as the container for the Preferences.
  */
 
-pContainer = new DefaultPersistenceStoreContainer(15000, 10000)
-if (parent != null) 
-{
-	pContainer.setParent(parent);
-}
-
-ComponentAdapter ca = new ConstructorComponentAdapter("jetspeed", PBStore, new Parameter[] {new ConstantParameter("jetspeed")})
-pContainer.registerComponent(ca)
-
 if(parent != null)
 {
 	container = new DefaultPicoContainer(parent)
-	parent.registerComponentInstance(PersistenceStoreContainer, pContainer);
 	parent.registerComponentImplementation(PropertyManager, PropertyManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	//parent.registerComponentImplementation(PreferencesProvider, PreferencesProviderImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed"), new ConstantParameter("org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl")})
-	parent.registerComponentInstance(PreferencesProvider, new PreferencesProviderImpl(pContainer, "jetspeed", "org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl"))	
+	parent.registerComponentImplementation(PreferencesProvider, PreferencesProviderImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed"), new ConstantParameter("org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl")} )
 }
 else
 {
 	container = new DefaultPicoContainer()
-	container.registerComponentInstance(PersistenceStoreContainer, pContainer);
-    container.registerComponentImplementation(PropertyManager, PropertyManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	//container.registerComponentImplementation(PreferencesProvider, PreferencesProviderImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed"), new ConstantParameter("org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl")})
-	container.registerComponentInstance(PreferencesProvider, new PreferencesProviderImpl(pContainer, "jetspeed", "org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl"))
+	container.registerComponentImplementation(PropertyManager, PropertyManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
+	container.registerComponentImplementation(PreferencesProvider, PreferencesProviderImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed"), new ConstantParameter("org.apache.jetspeed.prefs.impl.PreferencesFactoryImpl")} )	
 }	
 	
 // This will be an empty container if "parent" was not null
