865176c76a048fd4dae38f0b1c29892a63347ce1
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-11 15:11:58 +0000

    quick_build now just calls "maven allBuild"
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186055 13f79535-47bb-0310-9956-ffa450edef68

--- quick_build.bat
+++ quick_build.bat
@@ -8,46 +8,5 @@ set _JETSPEED_HOME=.
 echo "JETSPEED_HOME:" %_JETSPEED_HOME%
 
 
-echo
-echo +++++++++++++++++++++++++++
-echo 0. Clean targets
-echo +++++++++++++++++++++++++++
-
-call maven allClean
-
-echo
-
-echo +++++++++++++++++++++++++++
-echo 1. Recreate database
-echo +++++++++++++++++++++++++++
-
-cd portal
-call maven db.recreate
-call maven db.test.recreate
-cd ..
-
-echo
-echo +++++++++++++++++++++++++++
-echo 2. Build all Jetspeed sub projects
-echo +++++++++++++++++++++++++++
-
-call maven allBuild
-
-echo
-echo +++++++++++++++++++++++++++
-echo 4. Deploy Jetspeed Portal WAR into Catalina
-echo +++++++++++++++++++++++++++
-
-cd portal
-call maven deploy
-cd ..
-
-echo
-echo +++++++++++++++++++++++++++
-echo 5. Install the demo web application into Catalina
-echo +++++++++++++++++++++++++++
-
-cd portal
-call maven pam.deploy 
-cd ..
+maven allBuild
 
