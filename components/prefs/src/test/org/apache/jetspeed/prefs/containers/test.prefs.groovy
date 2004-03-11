3cf4c451252a260fcc076ff12246e7dc36ec7657
dlestrat <dlestrat@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-11 02:57:25 +0000

    Finally got a satisfying way to assemble containers and instantiate PreferencesProvider during assembly. Use getComponentInstanceOfType: Finds a component instance matching the type, looking in parent if not found in self (unless parent is null).
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186035 13f79535-47bb-0310-9956-ffa450edef68

--- components/prefs/src/test/org/apache/jetspeed/prefs/containers/test.prefs.groovy
+++ components/prefs/src/test/org/apache/jetspeed/prefs/containers/test.prefs.groovy
@@ -1,6 +1,7 @@
 import org.picocontainer.defaults.DefaultPicoContainer
 
 import org.apache.jetspeed.components.util.NanoQuickAssembler
+import org.apache.jetspeed.prefs.impl.PreferencesProviderImpl
 import java.io.File
 
 // WARNING!!!!!!
@@ -10,7 +11,6 @@ import java.io.File
 // in pico.  You need to use Class.forName() instead.
 
 
-
 // create the root container
 container = new DefaultPicoContainer()
 
@@ -18,10 +18,14 @@ ClassLoader cl = Thread.currentThread().getContextClassLoader()
 
 NanoQuickAssembler.assemble(cl, "org/apache/jetspeed/containers/rdbms.container.groovy", container)
 
+NanoQuickAssembler.assemble(cl, "org/apache/jetspeed/containers/persistence.container.groovy", container)
+
 //
 // Preferences
 //
-
 NanoQuickAssembler.assemble(cl, "org/apache/jetspeed/prefs/containers/prefs.container.groovy", container)
+
+// Instantiate the Preferences provider.
+container.getComponentInstanceOfType(PreferencesProviderImpl)
 
 return container
