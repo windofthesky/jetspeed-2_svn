1565b9b05d5c5ad6be3261305548a0c27131091a
jford <jford@13f79535-47bb-0310-9956-ffa450edef68>
2004-03-18 19:40:55 +0000

    Added Apache License 2.0
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186166 13f79535-47bb-0310-9956-ffa450edef68

--- src/sql/populate-entities-for-default-psml.sql
+++ src/sql/populate-entities-for-default-psml.sql
@@ -1,3 +1,19 @@
+-----------------------------------------------------------------------------
+-- Copyright 2004 The Apache Software Foundation
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+-- http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-----------------------------------------------------------------------------
+
 -- this is a temporary script to populate entities for the XML Page Registry
 INSERT INTO PORTLET_ENTITY (OID, PORTLET_DEFINITION_ID, ID) 
   VALUES (1, (SELECT ID FROM PORTLET_DEFINITION WHERE NAME = 'TwoColumns'), '1');
