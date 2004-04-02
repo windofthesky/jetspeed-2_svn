f47de07364980d7f3fbc526b1a48ec51afd18f6f
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2004-04-02 18:43:04 +0000

    removing PAM PA moving PAM portlets into jetspeed PA, resolving classloader issues PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186255 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/java/org/apache/jetspeed/portlets/pam/PortletApplicationBrowser.java
+++ portal/src/java/org/apache/jetspeed/portlets/pam/PortletApplicationBrowser.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jetspeed.apps.pam;
+package org.apache.jetspeed.portlets.pam;
 
 import java.io.IOException;
 //import java.io.InputStream;
