587b979e975ae5d0dd9f09198fcf0109d221b233
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-04-20 15:00:03 +0000

    cleaned up nanocontainer.groovy
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/branches/NANOCONTAINER_DEPLOYER@186330 13f79535-47bb-0310-9956-ffa450edef68

--- components/idgenerator/src/java/META-INF/nanocontainer.groovy
+++ components/idgenerator/src/java/META-INF/nanocontainer.groovy
@@ -14,65 +14,15 @@
  * limitations under the License.
  */
 import org.picocontainer.defaults.DefaultPicoContainer
-import org.picocontainer.defaults.ConstantParameter
-import org.picocontainer.ComponentAdapter
-import org.picocontainer.defaults.ConstructorInjectionComponentAdapter
-import org.picocontainer.Parameter
-import org.picocontainer.defaults.ComponentParameter
-import org.apache.jetspeed.locator.JetspeedTemplateLocator
-import org.apache.jetspeed.components.ComponentAssemblyTestCase
 import org.apache.jetspeed.idgenerator.JetspeedIdGenerator
-import org.apache.jetspeed.page.PageManager
-import org.apache.jetspeed.page.impl.CastorXmlPageManager
-import org.apache.jetspeed.Jetspeed
-import org.apache.jetspeed.components.persistence.store.PersistenceStoreContainer
-import org.apache.jetspeed.components.util.system.FSSystemResourceUtilImpl
-import org.apache.jetspeed.container.window.PortletWindowAccessor
-import org.apache.jetspeed.container.window.impl.PortletWindowAccessorImpl
-import org.apache.jetspeed.components.portletentity.PortletEntityAccessComponent
-import org.apache.jetspeed.components.portletregistry.PortletRegistryComponent
-
-import org.apache.jetspeed.cache.file.FileCache
-import org.apache.jetspeed.profiler.Profiler
-import org.apache.jetspeed.profiler.impl.JetspeedProfiler
-import org.apache.jetspeed.capability.Capabilities
-import org.apache.jetspeed.capability.impl.JetspeedCapabilities
-
-import org.apache.jetspeed.aggregator.PageAggregator
-import org.apache.jetspeed.aggregator.impl.PageAggregatorImpl
-import org.apache.jetspeed.aggregator.PortletAggregator
-import org.apache.jetspeed.aggregator.impl.PortletAggregatorImpl
-import org.apache.jetspeed.aggregator.PortletRenderer
-import org.apache.jetspeed.aggregator.impl.PortletRendererImpl
-import org.apache.pluto.PortletContainer
-import org.apache.jetspeed.request.RequestContextComponent
-import org.apache.jetspeed.request.JetspeedRequestContextComponent
-import org.apache.jetspeed.container.session.NavigationalStateComponent
-import org.apache.jetspeed.container.session.impl.JetspeedNavigationalStateComponent
-
-import org.apache.jetspeed.components.util.NanoQuickAssembler
-       
-// WARNING!!!!!!
-// DO NOT use {Class}.class as it appears to be broken in Groovy
-// You end getting a Class instance of the type java.lang.Class
-// instead of the requested type!  This causes AssignabilityExceptions
-// in pico.  You need to use Class.forName() instead.
-
-ClassLoader cl = Thread.currentThread().getContextClassLoader()
 
 
-// create the root container
 container = new DefaultPicoContainer()
 
-//
-// ID Generator
-//
 Long counterStart = 65536
 peidPrefix = "P-"
 peidSuffix = ""
 idgenerator = new JetspeedIdGenerator(counterStart, peidPrefix, peidSuffix)
 container.registerComponentInstance("IdGenerator", idgenerator)
-
-
 
 return container
