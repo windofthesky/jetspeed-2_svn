6ffa3503901c316fcb8c91f3f7224a7085f869a2
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-04-22 21:19:28 +0000

    - removed uneeded mappings - repository_security.xml moved to META-INF and renamed to ojb_repository.xml - now use ChildAwareContainer
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/branches/NANOCONTAINER_DEPLOYER@186376 13f79535-47bb-0310-9956-ffa450edef68

--- components/security/src/java/META-INF/nanocontainer.groovy
+++ components/security/src/java/META-INF/nanocontainer.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  * ========================================================================
  */
-import org.picocontainer.defaults.DefaultPicoContainer
+import org.apache.jetspeed.components.ChildAwareContainer
 import org.picocontainer.Parameter
 import org.picocontainer.defaults.ConstantParameter
 import org.picocontainer.defaults.ComponentParameter
@@ -37,33 +37,14 @@ import org.apache.jetspeed.security.impl.PermissionManagerImpl
 
 import java.io.File
 
-/**
- * This is the standard assembly for a Security
- * component.  We want the Security component to be exposed
- * at as high the container hierarchy as possibly so, if a
- * parent container is provided, we will regsiter to the parent
- * and use it as the container for the Security.
- */
 
-if(parent != null)
-{
-	container = new DefaultPicoContainer(parent)
-	parent.registerComponentImplementation(UserManager, UserManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	parent.registerComponentImplementation(GroupManager, GroupManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	parent.registerComponentImplementation(RoleManager, RoleManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	parent.registerComponentImplementation(PermissionManager, PermissionManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	parent.registerComponentImplementation(RdbmsPolicy, RdbmsPolicy, new Parameter[] {new ComponentParameter(PermissionManager)} )
-	parent.registerComponentImplementation(SecurityProvider, SecurityProviderImpl, new Parameter[] {new ConstantParameter("login.conf"), new ComponentParameter(RdbmsPolicy), new ComponentParameter(UserManager)})
-}
-else
-{
-	container = new DefaultPicoContainer()
-    container.registerComponentImplementation(UserManager, UserManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	container.registerComponentImplementation(GroupManager, GroupManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	container.registerComponentImplementation(RoleManager, RoleManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	container.registerComponentImplementation(PermissionManager, PermissionManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
-	container.registerComponentImplementation(SecurityProvider, SecurityProviderImpl, new Parameter[] {new ConstantParameter("login.conf"), new ComponentParameter(RdbmsPolicy), new ComponentParameter(UserManager)})
-}	
+
+container = new ChildAwareContainer()
+container.registerComponentImplementation(UserManager, UserManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
+container.registerComponentImplementation(GroupManager, GroupManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
+container.registerComponentImplementation(RoleManager, RoleManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
+container.registerComponentImplementation(PermissionManager, PermissionManagerImpl, new Parameter[] {new ComponentParameter(PersistenceStoreContainer), new ConstantParameter("jetspeed")} )
+container.registerComponentImplementation(SecurityProvider, SecurityProviderImpl, new Parameter[] {new ConstantParameter("login.conf"), new ComponentParameter(RdbmsPolicy), new ComponentParameter(UserManager)})
 	
-// This will be an empty container if "parent" was not null
+	
 return container
