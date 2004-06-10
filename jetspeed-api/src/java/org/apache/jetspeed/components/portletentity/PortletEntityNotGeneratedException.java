24dae44b7169f6f72e831ecf6b1fd71b0581e42f
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-06-10 20:07:49 +0000

    no exception
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186769 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jetspeed-api/src/java/org/apache/jetspeed/components/portletentity/PortletEntityNotGeneratedException.java
@@ -0,0 +1,57 @@
+/*
+ * Created on Jun 10, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.apache.jetspeed.components.portletentity;
+
+import org.apache.commons.lang.exception.NestableException;
+
+/**
+ * @author scott
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class PortletEntityNotGeneratedException extends NestableException
+{
+
+    /**
+     * 
+     */
+    public PortletEntityNotGeneratedException()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     */
+    public PortletEntityNotGeneratedException( String arg0 )
+    {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     */
+    public PortletEntityNotGeneratedException( Throwable arg0 )
+    {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     * @param arg1
+     */
+    public PortletEntityNotGeneratedException( String arg0, Throwable arg1 )
+    {
+        super(arg0, arg1);
+        // TODO Auto-generated constructor stub
+    }
+
+}
