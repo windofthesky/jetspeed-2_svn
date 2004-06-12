75084b56aab56dfe0fe24e0f46f722c9d9f65103
dlestrat <dlestrat@13f79535-47bb-0310-9956-ffa450edef68>
2004-06-12 16:39:24 +0000

    Javadoc + copyright.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186802 13f79535-47bb-0310-9956-ffa450edef68

--- applications/security/src/java/org/apache/jetspeed/portlets/security/AddUserAction.java
+++ applications/security/src/java/org/apache/jetspeed/portlets/security/AddUserAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.jetspeed.portlets.security;
 
 import javax.servlet.http.HttpServletRequest;
@@ -5,23 +20,37 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jetspeed.portlets.security.AddUserForm;
 
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class AddUserAction extends org.apache.struts.action.Action {
-    
+public class AddUserAction extends Action
+{
+
     // Local Forwards
     public static final String FORWARD_EDITUSER = "editUser";
-    
-    public AddUserAction() {
+
+    /**
+     * <p>Default constructor.</p>
+     */
+    public AddUserAction()
+    {
     }
-    
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        
-        String username = ((AddUserForm)form).getUsername();
-        String password = ((AddUserForm)form).getPassword();
-        
+
+    /**
+     * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm,
+     *      javax.servlet.http.HttpServletRequest,
+     *      javax.servlet.http.HttpServletResponse)
+     */
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception
+    {
+
+        String username = ((AddUserForm) form).getUsername();
+        String password = ((AddUserForm) form).getPassword();
+
         return mapping.findForward(FORWARD_EDITUSER);
     }
 
