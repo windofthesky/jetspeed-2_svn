30c047ad60e4fe420db2cf3fda7c9aba24c6e668
jford <jford@13f79535-47bb-0310-9956-ffa450edef68>
2004-06-19 19:27:54 +0000

    Added Apache License 2.0
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186830 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/java/org/apache/jetspeed/util/descriptor/PortletPreferenceRule.java
+++ portal/src/java/org/apache/jetspeed/util/descriptor/PortletPreferenceRule.java
@@ -1,8 +1,17 @@
 /*
- * Created on Jun 17, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.util.descriptor;
 
@@ -16,10 +25,7 @@ import org.apache.pluto.om.portlet.PortletApplicationDefinition;
 import org.xml.sax.Attributes;
 
 /**
- * @author scott
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * @author <a href="mailto:weaver@apache.org">Scott T. Weaver</a>
  */
 public class PortletPreferenceRule extends Rule
 {
