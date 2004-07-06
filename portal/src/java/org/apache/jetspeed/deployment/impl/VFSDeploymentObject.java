a23cafa6c65d7c5d995af19ae7c6c5807df1e3ca
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-06 23:16:19 +0000

    yes sir, call me  "license boy"
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    CVS: ----------------------------------------------------------------------
    CVS: PR:
    CVS:   If this change addresses a PR in the problem report tracking
    CVS:   database, then enter the PR number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system, such as NCSA,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to Apache by someone else; i.e.,
    CVS:   they sent us a patch or a new module, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186950 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/java/org/apache/jetspeed/deployment/impl/VFSDeploymentObject.java
+++ portal/src/java/org/apache/jetspeed/deployment/impl/VFSDeploymentObject.java
@@ -1,8 +1,17 @@
 /*
- * Created on Jun 25, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.deployment.impl;
 
@@ -17,13 +26,13 @@ import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.FileType;
+import org.apache.commons.vfs.impl.DefaultFileSystemManager;
 import org.apache.jetspeed.deployment.DeploymentObject;
 
 /**
- * @author scott
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * 
+ * @author <a href="mailto:weaver@apache.org">Scott T. Weaver </a>
+ * @version $Id$
  */
 public class VFSDeploymentObject implements DeploymentObject
 {
@@ -114,6 +123,8 @@ public class VFSDeploymentObject implements DeploymentObject
         fsObject.close();
         fsManager.getFilesCache().removeFile(fsStructure.getFileSystem(), fsStructure.getName());
         fsManager.getFilesCache().removeFile(fsObject.getFileSystem(), fsObject.getName());
+        //((DefaultFileSystemManager)fsManager).close();
+        //((DefaultFileSystemManager)fsManager).init();
     }
 
     /**
--- portal/src/test/org/apache/jetspeed/deployment/TestSimpleDeployment.java
+++ portal/src/test/org/apache/jetspeed/deployment/TestSimpleDeployment.java
@@ -1,8 +1,17 @@
-/**
- * Created on Jan 13, 2004
- *
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
  * 
- * @author
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.deployment;
 
@@ -107,8 +116,12 @@ public class TestSimpleDeployment extends RegistrySupportedTestCase
         // assertEquals("org.apache.commons.vfs.provider.jar.JarFileSystem", testObj.getFileSystem().getClass().getName());
         FileObject webXml = testObj.resolveFile("WEB-INF/web.xml");
         assertNotNull(webXml);
-        assertNotNull(webXml.getContent().getInputStream());       
+        assertNotNull(webXml.getContent().getInputStream());
+        //testObj.close();
+        //fsManager.close();
         
+        //demoApp.delete();
+        System.out.println("Done");
     }
     
     public void testFileSystemManagerOnDir() throws Exception
