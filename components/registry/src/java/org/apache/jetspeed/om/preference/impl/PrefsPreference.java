48b70cd7f6dc955b1576868ac548d7dcd90ced12
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-09 18:19:40 +0000

    added some docs
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186964 13f79535-47bb-0310-9956-ffa450edef68

--- components/registry/src/java/org/apache/jetspeed/om/preference/impl/PrefsPreference.java
+++ components/registry/src/java/org/apache/jetspeed/om/preference/impl/PrefsPreference.java
@@ -1,8 +1,17 @@
 /*
- * Created on Jun 11, 2004
+ * Copyright 2000-2001,2004 The Apache Software Foundation.
  * 
- * TODO To change the template for this generated file go to Window -
- * Preferences - Java - Code Generation - Code and Comments
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.om.preference.impl;
 
@@ -22,21 +31,7 @@ import org.apache.jetspeed.om.common.preference.PreferenceComposite;
 import org.apache.jetspeed.om.impl.PreferenceDescriptionImpl;
 import org.apache.pluto.om.common.Description;
 
-/*
- * Copyright 2000-2001,2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 public class PrefsPreference implements PreferenceComposite
 {
     protected static final String VALUES_PATH = "values";
@@ -525,6 +520,18 @@ public class PrefsPreference implements PreferenceComposite
             throw ise;
         }
     }
+    
+    /**
+     * 
+     * <p>
+     * createPrefenceNode
+     * </p>
+     * 
+     * Creates a Preferences object for this portlet
+     *
+     * @param portlet
+     * @return
+     */
     
     public static Preferences createPrefenceNode(PortletDefinitionComposite portlet)
     {
