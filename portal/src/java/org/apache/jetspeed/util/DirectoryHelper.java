967f3659cddf9ad80865a734501f9295293632b2
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-09 18:29:43 +0000

    Our replacement for the VFS-style logic we need
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@186978 13f79535-47bb-0310-9956-ffa450edef68

---
+++ portal/src/java/org/apache/jetspeed/util/DirectoryHelper.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2000-2001,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author <a href="mailto:weaver@apache.org">Scott T. Weaver</a>
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class DirectoryHelper implements FileSystemHelper
+{
+
+    protected File directory;
+
+    /**
+     * 
+     */
+    public DirectoryHelper(File directory)
+    {
+        super();
+        if(!directory.exists())
+        {
+            directory.mkdirs();
+        }
+        
+        if(!directory.isDirectory())
+        {
+            throw new IllegalArgumentException("DirectoryHelper(File) requires directory not a file.");
+        }
+        this.directory = directory;
+    }
+
+    /**
+     * <p>
+     * copyFrom
+     * </p>
+     *
+     * @see org.apache.jetspeed.util.FileSystemHelper#copyFrom(java.io.File)
+     * @param directory
+     * @throws IOException
+     */
+    public void copyFrom( File srcDirectory ) throws IOException
+    {
+        if(!directory.isDirectory())
+        {
+            throw new IllegalArgumentException("DirectoryHelper.copyFrom(File) requires directory not a file.");
+        }
+        copyFiles(srcDirectory, directory);
+    }
+    
+    /**
+     * 
+     * <p>
+     * copyFiles
+     * </p>
+     *
+     * @param srcDir Source directory to copy from.
+     * @param dstDir Destination directory to copy to.
+     * @throws IOException
+     * @throws FileNotFoundException
+
+     */
+    protected void copyFiles(File srcDir, File dstDir) throws IOException
+    {
+             
+        File[] children = srcDir.listFiles();
+        for(int i=0; i<children.length; i++)
+        {
+            File child = children[i];
+            if(child.isFile())
+            {
+                File toFile = new File(dstDir, child.getName());
+                toFile.createNewFile();
+                FileChannel srcChannel = new FileInputStream(child).getChannel();
+                FileChannel dstChannel = new FileOutputStream(toFile).getChannel();
+                dstChannel.transferFrom(srcChannel, 0, srcChannel.size());
+                srcChannel.close();
+                dstChannel.close();
+            }
+            else
+            {
+                File newSubDir = new File(dstDir, child.getName());
+                newSubDir.mkdir();
+                copyFiles(child, newSubDir);
+            }
+        }
+    }
+
+    /**
+     * <p>
+     * remove
+     * </p>
+     *
+     * @see org.apache.jetspeed.util.FileSystemHelper#remove()
+     * 
+     */
+    public boolean remove()
+    {
+        return doRemove(directory);
+    }
+    
+    /**
+     * 
+     * <p>
+     * doRemove
+     * </p>
+     *
+     * @param file
+     * @return <code>true</code> if the removal war successful, otherwise returns
+     * <code>false</code>.
+     */
+    protected boolean doRemove(File file)
+    {
+        if (file.isDirectory())
+		{
+			String[] children = file.list();
+			for (int i = 0; i < children.length; i++)
+			{
+				boolean success = doRemove(new File(file, children[i]));
+				if (!success)
+				{
+					return false;
+				}
+			}
+		}
+
+		// The directory is now empty so delete it OR it is a plain file
+		return file.delete();        
+    }
+
+    /**
+     * <p>
+     * getRootDirectory
+     * </p>
+     *
+     * @see org.apache.jetspeed.util.FileSystemHelper#getRootDirectory()
+     * @return
+     */
+    public File getRootDirectory()
+    {       
+        return directory;
+    }
+
+    /**
+     * <p>
+     * close
+     * </p>
+     *
+     * @see org.apache.jetspeed.util.FileSystemHelper#close()
+     * 
+     */
+    public void close()
+    {
+        // TODO Auto-generated method stub
+
+    }
+    /**
+     * <p>
+     * getSourcePath
+     * </p>
+     *
+     * @see org.apache.jetspeed.util.FileSystemHelper#getSourcePath()
+     * @return
+     */
+    public String getSourcePath()
+    {       
+        return getRootDirectory().getAbsolutePath();
+    }
+}
