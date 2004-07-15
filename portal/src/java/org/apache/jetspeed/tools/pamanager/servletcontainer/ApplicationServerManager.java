3dffba4c2a2e43eb872fe873e6f439aed4436cfd
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-15 14:30:11 +0000

    Added javadocs
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187015 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/java/org/apache/jetspeed/tools/pamanager/servletcontainer/ApplicationServerManager.java
+++ portal/src/java/org/apache/jetspeed/tools/pamanager/servletcontainer/ApplicationServerManager.java
@@ -21,32 +21,139 @@ import java.io.InputStream;
 import org.apache.commons.httpclient.HttpException;
 
 /**
+ * 
+ * <p>
+ * ApplicationServerManager
+ * </p>
+ * <p>
+ * Implementations of this interface are used primarily by the
+ * {@link org.apache.jetspeed.tools.pamanager.ApplicationServerPAM}
+ * to interact with the servlet container that is supporting the web
+ * appliaction portion of deployed the portlet applications.
+ * </p>
  * @author <a href="mailto:weaver@apache.org">Scott T. Weaver</a>
+ * @version $Id$
  *
  */
 public interface ApplicationServerManager
 {
+    /**
+     * 
+     * <p>
+     * start
+     * </p>
+     * Starts the application represented by the context path, <code>appPath</code>
+     *
+     * @param appPath path to restart
+     * @return container-specific status message
+     * @throws HttpException
+     * @throws IOException
+     */
     String start( String appPath ) throws HttpException, IOException;
-
+    
+    /**
+     * 
+     * <p>
+     * stop
+     * </p>
+     * Stops the application represented by the context path, <code>appPath</code>
+     * 
+     * @param appPath
+     * @return container-specific status message
+     * @throws HttpException
+     * @throws IOException
+     */
     String stop( String appPath ) throws HttpException, IOException;
-
+    
+    /**
+     * 
+     * <p>
+     * reload
+     * </p>
+     * Reloads the application represented by the context path, <code>appPath</code>.  This
+     * must included re-reading the web.xml and reloading all classpath resources.
+     *
+     * @param appPath
+     * @return container-specific status message
+     * @throws HttpException
+     * @throws IOException
+     */
     String reload( String appPath ) throws HttpException, IOException;
-
+    
+    /**
+     * 
+     * <p>
+     * remove
+     * </p>
+     * Removes the application represented by the context path, <code>appPath</cod
+     * @param appPath
+     * @return container-specific status message
+     * @throws HttpException
+     * @throws IOException
+     */
     String remove( String appPath ) throws HttpException, IOException;
-
+    
+    /**
+     * 
+     * <p>
+     * install
+     * </p>
+     * Installs the web application represented by the <code>warPath</code> into the parent servlet
+     * container using the specified <code>appPath</code> as the context. 
+     * 
+     * @param warPath
+     * @param contexPath
+     * @return
+     * @throws HttpException
+     * @throws IOException
+     */
     String install( String warPath, String contexPath ) throws HttpException, IOException;
-
+    
+    /**
+     * 
+     * <p>
+     * deploy
+     * </p>
+     *
+     * Deploys the contents of the InputStream, <code>is</code>, into the parent servlet
+     * container using the specified <code>appPath</code> as the context path.
+     * 
+     * @param appPath
+     * @param is
+     * @param size size (in bytes) of InputStream <code>is</code>
+     * @return
+     * @throws HttpException
+     * @throws IOException
+     */
     String deploy( String appPath, InputStream is, int size ) throws HttpException, IOException;
 
     /**
+     * 
+     * <p>
+     * getHostPort
+     * </p>
+     *
      * @return
      */
     int getHostPort();
 
     /**
+     * 
+     * <p>
+     * getHostUrl
+     * </p>
+     *
      * @return
      */
     String getHostUrl();
     
+    /**
+     * 
+     * <p>
+     * isConnected
+     * </p>
+     *
+     * @return
+     */
     boolean isConnected();
 }
