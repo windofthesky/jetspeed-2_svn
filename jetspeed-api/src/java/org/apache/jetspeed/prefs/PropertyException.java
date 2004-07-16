071eb8263e8ac1664f71604bf87ea0e3d55dc55e
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-16 19:28:18 +0000

    PropertyException moved to jetspeed-api
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187019 13f79535-47bb-0310-9956-ffa450edef68

--- jetspeed-api/src/java/org/apache/jetspeed/prefs/PropertyException.java
+++ jetspeed-api/src/java/org/apache/jetspeed/prefs/PropertyException.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jetspeed.prefs.impl;
+package org.apache.jetspeed.prefs;
 
 import org.apache.jetspeed.exception.JetspeedException;
 
