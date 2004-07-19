17f68dd82879cae1fe6375f643a0c3ed938bd56d
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-19 20:47:00 +0000

    Jetspeed command line deploy tool Takes a war file and rewrites the web.xml infusing the Jetspeed-specific servlet
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    CVS: ----------------------------------------------------------------------
    CVS: PR:
    CVS:   If this change addresses a PR in the problem report tracking
    CVS:   database, then enter the PR number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system, such as NCSA,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to Apache by someone else; i.e.,
    CVS:   they sent us a patch or a new module, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187025 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/deploy-tool/src/java/org/apache/jetspeed/tools/deploy/JetspeedDeploy.java
@@ -0,0 +1,189 @@
+package org.apache.jetspeed.tools.deploy;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class JetspeedDeploy 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar jsdeploy.jar INPUT OUTPUT");
+            System.exit(1);
+            return;
+        }
+
+        new JetspeedDeploy(args[0], args[1]);
+    }
+
+    private final byte[] buffer = new byte[4096];
+
+    public JetspeedDeploy(String inputName, String outputName) throws Exception 
+    {
+        JarInputStream jin = null;
+        JarOutputStream jout = null;
+        try 
+        {
+            jin = new JarInputStream(new FileInputStream(inputName));
+            jout = new JarOutputStream(new FileOutputStream(outputName), jin.getManifest());
+
+            // copy over all of the files in the input war to the output
+            // war except for web.xml and portlet.xml, which we parse for
+            // use later
+            Document webXml = null;
+            Document portletXml = null;
+            ZipEntry src;
+            while ((src = jin.getNextEntry()) != null) 
+            {
+                String target = src.getName();
+                if ("WEB-INF/web.xml".equals(target)) 
+                {
+                    System.out.println("Found web.xml");
+                    webXml = parseXml(jin);
+                } 
+                else if ("WEB-INF/portlet.xml".equals(target)) 
+                {
+                    System.out.println("Found WEB-INF/portlet.xml");
+                    portletXml = parseXml(jin);
+                } 
+                else 
+                {
+                    addFile(target, jin, jout);
+                }
+            }
+
+            if (webXml == null) 
+            {
+                throw new IllegalArgumentException("WEB-INF/web.xml");
+            }
+            if (portletXml == null) 
+            {
+                throw new IllegalArgumentException("WEB-INF/portlet.xml");
+            }
+            
+            JetspeedWebApplicationRewriter rewriter = new JetspeedWebApplicationRewriter(webXml, true);
+            rewriter.processWebXML();
+            
+            // mung the web.xml
+            //webXml.getRootElement().setAttribute("foo", "bar");
+
+            // write the web.xml and portlet.xml files
+            addFile("WEB-INF/web.xml", webXml, jout);
+            addFile("WEB-INF/portlet.xml", portletXml, jout);
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+
+            if(jin != null) 
+            {
+                try 
+                {
+                    jin.close();
+                    jin = null;
+                } 
+                catch (IOException e1) 
+                {
+                    // ignore
+                }
+            }
+            if(jout != null) {
+                try {
+                    jout.close();
+                    jout = null;
+                } catch (IOException e1) {
+                    // ignore
+                }
+            }
+            new File(outputName).delete();
+        }
+    }
+
+    private Document parseXml(InputStream jin) throws Exception {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document document = saxBuilder.build(new UncloseableInputStream(jin));
+        return document;
+    }
+
+    public void addFile(String path, InputStream source, JarOutputStream jos) throws IOException 
+    {
+        jos.putNextEntry(new ZipEntry(path));
+        try {
+            int count;
+            while ((count = source.read(buffer)) > 0) {
+                jos.write(buffer, 0, count);
+            }
+        } finally {
+            jos.closeEntry();
+        }
+    }
+
+    public void addFile(String path, Document source, JarOutputStream jos) throws IOException {
+        jos.putNextEntry(new ZipEntry(path));
+
+        XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+        try {
+            xmlOutputter.output(source, jos);
+        } finally {
+            jos.closeEntry();
+        }
+    }
+
+    private class UncloseableInputStream extends InputStream {
+        private final InputStream in;
+
+        public UncloseableInputStream(InputStream in) {
+            this.in = in;
+        }
+
+        public int read() throws IOException {
+            return in.read();
+        }
+
+        public int read(byte b[]) throws IOException {
+            return in.read(b);
+        }
+
+        public int read(byte b[], int off, int len) throws IOException {
+            return in.read(b, off, len);
+        }
+
+        public long skip(long n) throws IOException {
+            return in.skip(n);
+        }
+
+        public int available() throws IOException {
+            return in.available();
+        }
+
+        public void close() throws IOException {
+            // not closeable
+        }
+
+        public void mark(int readlimit) {
+            in.mark(readlimit);
+        }
+
+        public void reset() throws IOException {
+            in.reset();
+        }
+
+        public boolean markSupported() {
+            return in.markSupported();
+        }
+    }
+}
