c79b09e052f45137ecf2afe903667417486854c6
ate <ate@13f79535-47bb-0310-9956-ffa450edef68>
2004-07-29 22:25:51 +0000

    Replacement for the StrutsServletContextProviderImpl now implementing the new Portals Bridges common ServletContextProvider
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187141 13f79535-47bb-0310-9956-ffa450edef68

---
+++ commons/src/java/org/apache/jetspeed/portlet/ServletContextProviderImpl.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.portlet;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.apache.jetspeed.container.JetspeedPortletContext;
+import org.apache.portals.bridges.common.StrutsServletContextProvider;
+
+/**
+ * ServletContextProviderImpl supplies access to the
+ * Servlet context of a Jetspeed Portlet.
+ * @author <a href="mailto:ate@douma.nu">Ate Douma</a>
+ * @version $Id$
+ */
+public class ServletContextProviderImpl implements StrutsServletContextProvider 
+{
+    public ServletContext getServletContext(GenericPortlet portlet) 
+    {
+        return ((JetspeedPortletContext)portlet.getPortletContext()).getServletContext();
+    }
+
+    public HttpServletRequest getHttpServletRequest(GenericPortlet portlet, PortletRequest request) 
+    {
+        return (HttpServletRequest) ((HttpServletRequestWrapper) request).getRequest();
+    }
+
+    public HttpServletResponse getHttpServletResponse(GenericPortlet portlet, PortletResponse response) 
+    {
+        return (HttpServletResponse) ((HttpServletResponseWrapper) response).getResponse();
+    }
+}
