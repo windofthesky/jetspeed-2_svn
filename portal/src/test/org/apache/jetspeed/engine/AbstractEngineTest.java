08e3b9ab6232f01cec9b6d900ae05a19169608b4
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-08-02 20:31:23 +0000

    added asl 2.0
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187183 13f79535-47bb-0310-9956-ffa450edef68

--- portal/src/test/org/apache/jetspeed/engine/AbstractEngineTest.java
+++ portal/src/test/org/apache/jetspeed/engine/AbstractEngineTest.java
@@ -1,8 +1,17 @@
 /*
- * Created on Jul 16, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Copyright 2000-2001,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.jetspeed.engine;
 
@@ -61,7 +70,7 @@ public abstract class AbstractEngineTest extends TestCase
     {        
         super.setUp();
         // need to flag internal JNDI on...
-        System.setProperty("portal.use.internal.jndi", "true");
+        System.setProperty(AbstractEngine.JNDI_SUPPORT_FLAG_KEY, "true");
         PropertiesConfiguration config = new  PropertiesConfiguration();
         config.load(new FileInputStream("./src/webapp/WEB-INF/conf/jetspeed.properties")); 
         Mock servletConfigMock = new Mock(ServletConfig.class);
