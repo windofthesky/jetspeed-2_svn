f40cbc3d288ad9f3d544a021d75870af38e91323
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-17 20:02:03 +0000

    decoupled profiler from page manager, moving profile locator into locator package implementationby Randy Watler
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    CVS: ----------------------------------------------------------------------
    CVS: PR:
    CVS:   If this change addresses a PR in the problem report tracking
    CVS:   database, then enter the PR number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system, such as NCSA,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to Apache by someone else; i.e.,
    CVS:   they sent us a patch or a new module, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/locator/src/java/org/apache/jetspeed/profiler/impl/JetspeedProfiledPageContext.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.profiler.impl;
+
+import org.apache.jetspeed.om.folder.Folder;
+import org.apache.jetspeed.om.page.Page;
+import org.apache.jetspeed.page.document.NodeSet;
+import org.apache.jetspeed.profiler.ProfileLocator;
+import org.apache.jetspeed.profiler.ProfiledPageContext;
+import org.apache.jetspeed.profiler.Profiler;
+
+/**
+ * JetspeedProfiledPageContext
+ *
+ * @author <a href="mailto:rwatler@finali.com">Randy Watler</a>
+ * @version $Id$
+ */
+public class JetspeedProfiledPageContext implements ProfiledPageContext
+{    
+    private ProfileLocator locator;
+    private Page page;
+    private Folder folder;
+    private NodeSet siblingPages;
+    private Folder parentFolder;
+    private NodeSet siblingFolders;
+    private NodeSet rootLinks;
+
+    public void init(Profiler profiler, ProfileLocator locator)
+    {
+        // save profiled context supplied by profiler
+        this.locator = locator;
+    }
+
+    public ProfileLocator getLocator()
+    {
+        return locator;
+    }
+
+    public Page getPage()
+    {
+        return page;
+    }
+
+    public void setPage(Page page)
+    {
+        this.page = page;
+    }
+
+    public Folder getFolder()
+    {
+        return folder;
+    }
+
+    public void setFolder(Folder folder)
+    {
+        this.folder = folder;
+    }
+
+    public NodeSet getSiblingPages()
+    {
+        return siblingPages;
+    }
+
+    public void setSiblingPages(NodeSet pages)
+    {
+        this.siblingPages = pages;
+    }
+
+    public Folder getParentFolder()
+    {
+        return parentFolder;
+    }
+
+    public void setParentFolder(Folder folder)
+    {
+        this.parentFolder = folder;
+    }
+
+    public NodeSet getSiblingFolders()
+    {
+        return siblingFolders;
+    }
+
+    public void setSiblingFolders(NodeSet folders)
+    {
+        this.siblingFolders = folders;
+    }
+
+    public NodeSet getRootLinks()
+    {
+        return rootLinks;
+    }
+
+    public void setRootLinks(NodeSet links)
+    {
+        this.rootLinks = links;
+    }
+}
