4ec02a8f514f90705965311c44eb8460205621f1
dlestrat <dlestrat@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-18 19:32:34 +0000

    Formatting.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187538 13f79535-47bb-0310-9956-ffa450edef68

--- components/rdbms/src/java/org/apache/jetspeed/components/util/DatasourceTestCase.java
+++ components/rdbms/src/java/org/apache/jetspeed/components/util/DatasourceTestCase.java
@@ -23,16 +23,18 @@ import org.apache.jetspeed.components.jndi.JNDIComponent;
 import org.apache.jetspeed.components.jndi.TyrexJNDIComponent;
 
 /**
- * @author <a href="mailto:sweaver@einnovation.com">Scott T. Weaver</a>
- *
+ * @author <a href="mailto:sweaver@einnovation.com">Scott T. Weaver </a>
+ *  
  */
 public class DatasourceTestCase extends TestCase
 {
 
     protected BoundDBCPDatasourceComponent datasourceComponent;
+
     protected JNDIComponent jndi;
+
     /**
-     * 
+     *  
      */
     public DatasourceTestCase()
     {
@@ -43,29 +45,30 @@ public class DatasourceTestCase extends TestCase
     /**
      * @param arg0
      */
-    public DatasourceTestCase( String arg0 )
+    public DatasourceTestCase(String arg0)
     {
         super(arg0);
         // TODO Auto-generated constructor stub
     }
 
-
-    /* (non-Javadoc)
+    /**
      * @see junit.framework.TestCase#setUp()
      */
     protected void setUp() throws Exception
-    {        
+    {
         super.setUp();
         jndi = new TyrexJNDIComponent();
         String url = System.getProperty("org.apache.jetspeed.database.url");
         String driver = System.getProperty("org.apache.jetspeed.database.driver");
         String user = System.getProperty("org.apache.jetspeed.database.user");
         String password = System.getProperty("org.apache.jetspeed.database.password");
-        datasourceComponent = new BoundDBCPDatasourceComponent(user, password, driver, url, 20, 5000, GenericObjectPool.WHEN_EXHAUSTED_GROW,  true, "jetspeed", jndi );
+        datasourceComponent = new BoundDBCPDatasourceComponent(user, password, driver, url, 20, 5000,
+                GenericObjectPool.WHEN_EXHAUSTED_GROW, true, "jetspeed", jndi);
         datasourceComponent.start();
-        
+
     }
-    /* (non-Javadoc)
+
+    /**
      * @see junit.framework.TestCase#tearDown()
      */
     protected void tearDown() throws Exception
@@ -73,5 +76,5 @@ public class DatasourceTestCase extends TestCase
         jndi.unbindFromCurrentThread();
         super.tearDown();
     }
-    
+
 }
