0d237b3e6c200a7cef28054069d3f10acb74234b
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-23 22:46:45 +0000

    new rule for role + group fallback
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    CVS: ----------------------------------------------------------------------
    CVS: PR:
    CVS:   If this change addresses a PR in the problem report tracking
    CVS:   database, then enter the PR number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system, such as NCSA,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to Apache by someone else; i.e.,
    CVS:   they sent us a patch or a new module, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187598 13f79535-47bb-0310-9956-ffa450edef68

--- src/sql/populate-db-default.sql
+++ src/sql/populate-db-default.sql
@@ -298,6 +298,13 @@ insert into PROFILING_RULE values ('path',
    'use a path to locate.');
 insert into RULE_CRITERION values (10, 'path', 0, 'path', 'path', '/', 0);
 
+insert into PROFILING_RULE values ('role-group', 
+     'org.apache.jetspeed.profiler.rules.impl.RoleFallbackProfilingRule',
+     'A role based fallback algorithm that searches all groups and roles for a user');
+insert into RULE_CRITERION values (11, 'role-group', 0, 'role', 'role', null, 2);
+insert into RULE_CRITERION values (12, 'role-group', 1, 'group', 'group', null, 2);
+insert into RULE_CRITERION values (13, 'role-group', 1, 'request.session', 'page', 'default-page', 0);
+
 insert into PRINCIPAL_RULE_ASSOC values ( 'guest', 'j1' );
 insert into PRINCIPAL_RULE_ASSOC values ( 'jetspeed', 'role-fallback' );
 
