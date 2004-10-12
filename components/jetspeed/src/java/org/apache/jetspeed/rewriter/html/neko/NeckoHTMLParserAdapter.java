e68dbb7018049c0041d6b6bd71a559d7e4ad3ecd
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-10-12 19:38:14 +0000

    see: http://nagoya.apache.org/jira/browse/JS2-138#action_53952
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187689 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/jetspeed/src/java/org/apache/jetspeed/rewriter/html/neko/NeckoHTMLParserAdapter.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2000-2001,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.rewriter.html.neko;
+
+import java.io.Reader;
+import java.io.Writer;
+
+import org.apache.jetspeed.rewriter.ParserAdaptor;
+import org.apache.jetspeed.rewriter.Rewriter;
+import org.apache.jetspeed.rewriter.RewriterException;
+
+/**
+ * <p>
+ * NeckoHTMLParserAdapter
+ * </p>
+ * <p>
+ *  
+ * </p>
+ * @author <a href="mailto:weaver@apache.org">Scott T. Weaver</a>
+ * @version $Id$
+ *
+ */
+public class NeckoHTMLParserAdapter implements ParserAdaptor
+{
+
+  
+    /**
+     * <p>
+     * parse
+     * </p>
+     *
+     * @see org.apache.jetspeed.rewriter.ParserAdaptor#parse(org.apache.jetspeed.rewriter.Rewriter, java.io.Reader)
+     * @param rewriter
+     * @param reader
+     * @throws RewriterException
+     */
+    public void parse( Rewriter rewriter, Reader reader ) throws RewriterException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * <p>
+     * rewrite
+     * </p>
+     *
+     * @see org.apache.jetspeed.rewriter.ParserAdaptor#rewrite(org.apache.jetspeed.rewriter.Rewriter, java.io.Reader, java.io.Writer)
+     * @param rewriter
+     * @param reader
+     * @param writer
+     * @throws RewriterException
+     */
+    public void rewrite( Rewriter rewriter, Reader reader, Writer writer ) throws RewriterException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
