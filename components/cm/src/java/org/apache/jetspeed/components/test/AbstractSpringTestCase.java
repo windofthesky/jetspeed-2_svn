d208422b30b9b64e75cbf77f8a7538bf6f0f8a17
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2004-10-29 13:52:22 +0000

    Spring supported testcase.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@187826 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/cm/src/java/org/apache/jetspeed/components/test/AbstractSpringTestCase.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2000-2001,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.components.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ * AbstractSpringTestCase
+ * </p>
+ * <p>
+ * 
+ * </p>
+ * 
+ * @author <a href="mailto:weaver@apache.org">Scott T. Weaver </a>
+ * @version $Id$
+ *  
+ */
+public abstract class AbstractSpringTestCase extends TestCase
+{
+    /**
+     * Provides access to the Spring ApplicationContext.
+     */
+    protected ClassPathXmlApplicationContext ctx;
+    
+    protected abstract String[] getConfigurations();
+    
+    
+
+    protected void setUp() throws Exception
+    {        
+        super.setUp();
+        ctx = new ClassPathXmlApplicationContext(getConfigurations());
+    }
+}
