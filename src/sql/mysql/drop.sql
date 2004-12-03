3a974f27aa37a906b186e54c8a22b897a947ff31
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2004-12-03 22:00:23 +0000

    - added schema support for MySQL 4 InnoDB - added drop.sql for Oracle to match current dbs
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@188055 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/sql/mysql/drop.sql
@@ -0,0 +1,12 @@
+drop table if exists RULE_CRITERION;
+drop table if exists PRINCIPAL_PERMISSION;
+drop table if exists PA_METADATA_FIELDS;
+drop table if exists PD_METADATA_FIELDS;
+drop table if exists USER_ATTRIBUTE_REF;
+drop table if exists USER_ATTRIBUTE;
+drop table if exists SECURITY_CREDENTIAL;
+drop table if exists SSO_SITE_TO_PRINCIPALS;
+drop table if exists SSO_PRINCIPAL_TO_REMOTE_PRINCIPAL;
+drop table if exists SECURITY_USER_ROLE;
+drop table if exists SECURITY_USER_GROUP;
+drop table if exists SECURITY_GROUP_ROLE;
