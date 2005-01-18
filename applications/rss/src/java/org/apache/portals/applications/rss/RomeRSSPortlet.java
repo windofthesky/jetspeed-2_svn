f93f437861a17ab6314b2e59e40c2cbfbff5b1bd
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2005-01-18 17:43:49 +0000

    http://issues.apache.org/jira/browse/JS2-191
    completed implementation of Rome RSS portlet
    contributed by Christophe Lombart
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@188215 13f79535-47bb-0310-9956-ffa450edef68

--- applications/rss/src/java/org/apache/portals/applications/rss/RomeRSSPortlet.java
+++ applications/rss/src/java/org/apache/portals/applications/rss/RomeRSSPortlet.java
@@ -15,26 +15,25 @@
  */
 package org.apache.portals.applications.rss;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
-import org.apache.portals.applications.transform.TransformCacheEntry;
-import org.apache.portals.applications.util.Streams;
-import org.w3c.dom.Document;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.portals.bridges.velocity.GenericVelocityPortlet;
+
+import org.apache.velocity.context.Context;
 
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
-import com.sun.syndication.io.SyndFeedOutput;
 import com.sun.syndication.io.XmlReader;
 
 /**
@@ -43,72 +42,75 @@ import com.sun.syndication.io.XmlReader;
  * @author <a href="mailto:taylor@apache.org">David Sean Taylor </a>
  * @version $Id$
  */
-public class RomeRSSPortlet extends AbstractRssPortlet
+public class RomeRSSPortlet extends GenericVelocityPortlet
 {
+
+    protected Log log = LogFactory.getLog(RomeRSSPortlet.class);
+
+    /**
+     * @see javax.portlet.Portlet#init(javax.portlet.PortletConfig)
+     */
     public void init(PortletConfig config) throws PortletException
     {
         super.init(config);
+
     }
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.portlet.GenericPortlet#doView(javax.portlet.RenderRequest,
-     *      javax.portlet.RenderResponse)
+
+    /**
+     * @see javax.portlet.GenericPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
      */
     public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException
     {
-        response.setContentType("text/html");
-        
-        PortletPreferences prefs = request.getPreferences();        
-        String url = prefs.getValue("url", "http://www.npr.org/rss/rss.php?topicId=4");
-        String stylesheet = getPortletConfig().getInitParameter("stylesheet");
-        String realStylesheet = getPortletConfig().getPortletContext().getRealPath(stylesheet);
 
-        URL xslt = getPortletConfig().getPortletContext().getResource(stylesheet);
-        
-        String key = cache.constructKey(url, stylesheet); // TODO: use the entire parameter list
-        TransformCacheEntry entry = cache.get(key);
-        if (entry != null)
+        response.setContentType("text/html");
+        Context velocityContext = this.getContext(request);
+        PortletPreferences prefs = request.getPreferences();
+        String url = prefs.getValue("url", "http://www.npr.org/rss/rss.php?topicId=4");
+        try
         {
-            byte[] bytes = (byte[])entry.getDocument();
-            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-            Streams.drain(bais, response.getPortletOutputStream());
-            bais.close();
+            URL feedUrl = new URL(url);
+            SyndFeedInput input = new SyndFeedInput();
+
+            SyndFeed feed = input.build(new XmlReader(feedUrl));
+
+            RssInfo rssInfo = new RssInfo(feed, new Integer(prefs.getValue("itemdisplayed", "15")).intValue(), new Boolean(prefs
+                    .getValue("openinpopup", "true")).booleanValue(), new Boolean(prefs.getValue("showdescription", "true"))
+                    .booleanValue(), new Boolean(prefs.getValue("showtitle", "true")).booleanValue(), new Boolean(prefs.getValue(
+                    "showtextinput", "true")).booleanValue());
+
+            velocityContext.put("rssInfo", rssInfo);
+
+            super.doView(request, response);
+
         }
-        else
-        {        
-            try
-            {
-                URL feedUrl = new URL(url);
-                SyndFeedInput input = new SyndFeedInput();
-                SyndFeed feed = input.build(new XmlReader(feedUrl));
-                SyndFeedOutput output = new SyndFeedOutput();            
-                //output.output(feed, response.getWriter());
-                Document document = output.outputW3CDom(feed);
-    
-                Map parameters = new HashMap();
-                
-                parameters.put("itemdisplayed", prefs.getValue("itemdisplayed", "15"));
-                parameters.put("openinpopup", prefs.getValue("openinpopup", "true"));
-                parameters.put("showdescription", prefs.getValue("showdescription", "true"));
-                parameters.put("showtitle", prefs.getValue("showtitle", "true"));
-                parameters.put("showtextinput", prefs.getValue("showtextinput", "true"));
-                
-                
-                // TODO: don't use a transform, instead use Velocity template and populate via Rome model
-                
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();                
-                transform.transform(realStylesheet, document, baos, parameters); 
-                byte[] bytes = baos.toByteArray();
-                ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-                Streams.drain(bais, response.getPortletOutputStream());                
-                cache.put(key, bytes, 15);
-            }
-            catch (Exception e)
-            {
-                response.getPortletOutputStream().write(new String("Failed to process RSS Feed: " + url + ", " + e).getBytes());
-            }
+        catch (Exception e)
+        {
+            throw new PortletException(new String("Failed to process RSS Feed: " + url + ", " + e));
+        }
+
+    }
+
+    /**
+     * 
+     * @see javax.portlet.GenericPortlet#doEdit(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+     */
+    public void doEdit(RenderRequest request, RenderResponse response) throws PortletException, IOException
+    {
+        response.setContentType("text/html");
+        doPreferencesEdit(request, response);
+    }
+
+    /**
+     * 
+     * @see javax.portlet.Portlet#processAction(javax.portlet.ActionRequest, javax.portlet.ActionResponse)
+     */
+    public void processAction(ActionRequest request, ActionResponse actionResponse) throws PortletException, java.io.IOException
+    {
+        String add = request.getParameter("Save");
+        if (add != null)
+        { 
+            processPreferencesAction(request, actionResponse);
         }
     }
+
 }
