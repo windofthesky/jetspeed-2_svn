89ccc6f59b68efcd036952c6622c67e549fd09c9
ate <ate@13f79535-47bb-0310-9956-ffa450edef68>
2005-01-27 13:38:43 +0000

    Houston: we have Tiles support!
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@188271 13f79535-47bb-0310-9956-ffa450edef68

---
+++ portals-bridges/struts/src/java/org/apache/portals/bridges/struts/PortletTilesRequestProcessor.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.portals.bridges.struts;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.tiles.TilesRequestProcessor;
+
+/**
+ * PortletTilesRequestProcessor
+ * 
+ * @author <a href="mailto:ate@douma.nu">Ate Douma</a>
+ * @version $Id$
+ */
+public class PortletTilesRequestProcessor extends TilesRequestProcessor 
+{
+    public PortletTilesRequestProcessor()
+    {
+        super();
+    }
+
+    public void process(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        if (PortletServlet.isPortletRequest(request))
+        {
+            if (!(response instanceof PortletServletResponseWrapper))
+            {
+                response = new PortletServletResponseWrapper(request, response);
+            }
+        }
+        super.process(request, response);
+    }
+    protected boolean processRoles(HttpServletRequest request,
+            HttpServletResponse response, ActionMapping mapping)
+            throws IOException, ServletException
+    {
+        boolean proceed = super.processRoles(request, response, mapping);
+        if (proceed &&
+                PortletServlet.isPortletRequest(request) &&
+                ((PortletServlet) super.servlet).performActionRenderRequest(request, response, mapping))
+        {
+            return false;
+        } else
+            return proceed;
+    }
+}
