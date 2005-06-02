6c77948d1d38d32c8670b81472615df35270b886
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2005-06-02 06:23:36 +0000

    Made JPT configurable again Added JPT interface and default impl
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@188636 13f79535-47bb-0310-9956-ffa450edef68

--- jetspeed-api/src/java/org/apache/jetspeed/CommonPortletServices.java
+++ jetspeed-api/src/java/org/apache/jetspeed/CommonPortletServices.java
@@ -36,4 +36,5 @@ public interface CommonPortletServices
     public final static String CPS_DEPLOYMENT_MANAGER_COMPONENT = "cps:DeploymentManager";    
     public final static String CPS_ENTITY_ACCESS_COMPONENT = "cps:EntityAccessor";
     public final static String CPS_ID_GENERATOR_COMPONENT = "cps:IdGenerator";
+    public final static String CPS_JETSPEED_POWERTOOL_FACTORY = "cps:Powertools";
 }
