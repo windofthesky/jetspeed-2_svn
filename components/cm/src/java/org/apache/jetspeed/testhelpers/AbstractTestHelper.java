d3945641231ea929a2df0514c5849b99056bcb8d
weaver <weaver@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-28 14:16:55 +0000

    Hopefully this will work in everyone's development environment.
    I added checks to make sure the properties file exists before trying to load them.
    I have also added the loading of ${PROJECT_HOME}/build.properties and ${PROJECT_HOME}/project.properties.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@225779 13f79535-47bb-0310-9956-ffa450edef68

--- components/cm/src/java/org/apache/jetspeed/testhelpers/AbstractTestHelper.java
+++ components/cm/src/java/org/apache/jetspeed/testhelpers/AbstractTestHelper.java
@@ -3,6 +3,8 @@ package org.apache.jetspeed.testhelpers;
 import java.io.File;
 import java.util.Map;
 
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -13,17 +15,41 @@ public abstract class AbstractTestHelper implements TestHelper
     public static final String APP_CONTEXT = "AppContext";
     private final Map context;
 
-    private static final PropertiesConfiguration USER_PROPERTIES;
+    private static final CompositeConfiguration USER_PROPERTIES;
     static
     {
         try
         {
-            USER_PROPERTIES= new PropertiesConfiguration(new File(System.getProperty("user.home"), "build.properties"));
+            File userBuildFile = new File(System.getProperty("user.home"), "build.properties");
+            Configuration userBuildProps = loadConfiguration(userBuildFile);
+            
+            File mavenBuildFile = new File("../../build.properties");
+            Configuration mavenBuildProps = loadConfiguration(userBuildFile);
+            
+            File mavenProjectFile = new File("../../project.properties");
+            Configuration mavenProjectProps = loadConfiguration(userBuildFile);
+            
+            USER_PROPERTIES = new CompositeConfiguration();
+            USER_PROPERTIES.addConfiguration(userBuildProps);
+            USER_PROPERTIES.addConfiguration(mavenBuildProps);
+            USER_PROPERTIES.addConfiguration(mavenProjectProps);            
         }
         catch (ConfigurationException e)
         {
             
            throw new IllegalStateException("Unable to load ${USER_HOME}/build.properties");
+        }
+    }
+
+    private static Configuration loadConfiguration(File propsFile) throws ConfigurationException
+    {
+        if(propsFile.exists())
+        {
+            return new PropertiesConfiguration(propsFile);
+        }
+        else
+        {
+            return new PropertiesConfiguration();
         }
     }   
 
