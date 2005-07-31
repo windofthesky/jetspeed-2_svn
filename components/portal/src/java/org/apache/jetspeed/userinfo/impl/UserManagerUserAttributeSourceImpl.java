6e622c5e84d602a9f2395f001180e013f36fae7d
dlestrat <dlestrat@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-31 15:37:43 +0000

    Formatting.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@226649 13f79535-47bb-0310-9956-ffa450edef68

--- components/portal/src/java/org/apache/jetspeed/userinfo/impl/UserManagerUserAttributeSourceImpl.java
+++ components/portal/src/java/org/apache/jetspeed/userinfo/impl/UserManagerUserAttributeSourceImpl.java
@@ -1,17 +1,17 @@
 /* Copyright 2004 Apache Software Foundation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jetspeed.userinfo.impl;
 
 import java.security.Principal;
@@ -36,8 +36,7 @@ import org.apache.jetspeed.userinfo.UserAttributeRetrievalException;
 import org.apache.jetspeed.userinfo.UserAttributeSource;
 
 /**
- * Default implementation of a UserAttribute source 
- * Provides users attributes from standard prefs implementation
+ * Default implementation of a UserAttribute source Provides users attributes from standard prefs implementation
  * 
  * @author <a href="mailto:KeithGarry.Boyce@bcbsma.com">Keith Garry Boyce</a>
  * @author <a href="mailto:taylor@apache.org">David Sean Taylor</a>
@@ -47,8 +46,7 @@ public class UserManagerUserAttributeSourceImpl implements UserAttributeSource
 {
 
     /** Logger */
-    private static final Log log = LogFactory
-            .getLog(UserManagerUserAttributeSourceImpl.class);
+    private static final Log log = LogFactory.getLog(UserManagerUserAttributeSourceImpl.class);
 
     /** The user manager */
     private UserManager userManager;
@@ -65,17 +63,14 @@ public class UserManagerUserAttributeSourceImpl implements UserAttributeSource
     /*
      * (non-Javadoc)
      * 
-     * @see org.jetspeed.userinfo.UserAttributeSource#getUserAttributeMap(javax.security.auth.Subject,
-     *      java.util.Set)
+     * @see org.jetspeed.userinfo.UserAttributeSource#getUserAttributeMap(javax.security.auth.Subject, java.util.Set)
      */
-    public Map getUserAttributeMap(Subject subject,
-            Collection userAttributeRefs, RequestContext context)
+    public Map getUserAttributeMap(Subject subject, Collection userAttributeRefs, RequestContext context)
             throws UserAttributeRetrievalException
     {
 
         Map userAttributeMap = new HashMap();
-        Principal userPrincipal = SecurityHelper.getPrincipal(subject,
-                UserPrincipal.class);
+        Principal userPrincipal = SecurityHelper.getPrincipal(subject, UserPrincipal.class);
         if (null != userPrincipal)
         {
             log.debug("Got user principal: " + userPrincipal.getName());
@@ -85,27 +80,21 @@ public class UserManagerUserAttributeSourceImpl implements UserAttributeSource
                 {
                     User user = userManager.getUser(userPrincipal.getName());
                     Preferences userInfoPrefs = user.getPreferences();
-                    for (Iterator iter = userAttributeRefs.iterator(); 
-                                        iter.hasNext();)
+                    for (Iterator iter = userAttributeRefs.iterator(); iter.hasNext();)
                     {
-                        UserAttributeRef currentAttributeRef = 
-                            (UserAttributeRef) iter.next();
-                        Object value = userInfoPrefs.get(currentAttributeRef
-                                .getName(), null);
+                        UserAttributeRef currentAttributeRef = (UserAttributeRef) iter.next();
+                        Object value = userInfoPrefs.get(currentAttributeRef.getName(), null);
                         if (value != null)
                         {
-                            userAttributeMap.put(currentAttributeRef.getName(),
-                                    value);
+                            userAttributeMap.put(currentAttributeRef.getName(), value);
                         }
 
                     }
                 }
-            } catch (SecurityException sex)
+            }
+            catch (SecurityException sex)
             {
-                log
-                        .warn(
-                                "Unexpected SecurityException in UserInfoManager",
-                                sex);
+                log.warn("Unexpected SecurityException in UserInfoManager", sex);
             }
         }
 
