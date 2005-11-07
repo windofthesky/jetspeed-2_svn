374f764fef3b4c41eefbe9aa85f6777fe404cdec
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2005-11-07 18:09:27 +0000

    http://issues.apache.org/jira/browse/JS2-362
    clean up, still work in progress, should finish up today
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@331559 13f79535-47bb-0310-9956-ffa450edef68

--- jetspeed-api/src/java/org/apache/jetspeed/statistics/AggregateStatistics.java
+++ jetspeed-api/src/java/org/apache/jetspeed/statistics/AggregateStatistics.java
@@ -1,22 +1,32 @@
-/*
- * Created on Nov 3, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+/* Copyright 2005 Apache Software Foundation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.apache.jetspeed.statistics;
 
 import java.io.Serializable;
-
+import java.util.Map;
 
 /**
- * @author david
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * AggregateStatistics
+ * 
+ * @author <a href="mailto:chris@bluesunrise.com">Chris Schaefer</a>
+ * @author <a href="mailto:taylor@apache.org">David Sean Taylor</a>
+ * @version $Id: $
  */
 public interface AggregateStatistics extends Serializable
 {
+    public void addRow(Map row);
     public int getHitCount();
     public float getAvgProcessingTime();
     public float getMinProcessingTime();
