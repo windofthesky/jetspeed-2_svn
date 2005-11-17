d64c846ac8d309725a92fd0079042038937c8b05
ate <ate@13f79535-47bb-0310-9956-ffa450edef68>
2005-11-17 00:33:02 +0000

    Replaced OJBHelper datasource setup with JNDI based datasource setup using new JetspeedTestJNDIComponent. to support Derby db based testcases (as well as all other dbms). NB: OJBHelper now is no longer in use.
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@345158 13f79535-47bb-0310-9956-ffa450edef68

--- components/portal/src/test/org/apache/jetspeed/testhelpers/SpringEngineHelper.java
+++ components/portal/src/test/org/apache/jetspeed/testhelpers/SpringEngineHelper.java
@@ -2,51 +2,48 @@ package org.apache.jetspeed.testhelpers;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.jetspeed.PortalTestConstants;
 import org.apache.jetspeed.components.SpringComponentManager;
 import org.apache.jetspeed.components.factorybeans.ServletConfigFactoryBean;
+import org.apache.jetspeed.components.jndi.JetspeedTestJNDIComponent;
 import org.apache.jetspeed.engine.Engine;
 import org.apache.jetspeed.engine.JetspeedEngine;
 import org.apache.jetspeed.mocks.ResourceLocatingServletContext;
-import org.springframework.context.ApplicationContext;
 
 import com.mockrunner.mock.web.MockServletConfig;
 
-public class SpringEngineHelper extends OJBHelper
+public class SpringEngineHelper extends AbstractTestHelper
 {
-
     public static final String ENGINE_ATTR = "Engine";     
     
+    protected JetspeedTestJNDIComponent jndiDS;
     
     public SpringEngineHelper(Map context)
     {
-        super(context);     
+        super(context);
     }
     
     private Engine engine;
 
     public void setUp() throws Exception
     {
-        super.setUp();
+        jndiDS = new JetspeedTestJNDIComponent();
+        jndiDS.setup();
+
         PropertiesConfiguration config = new PropertiesConfiguration();
         config.load(new FileInputStream(PortalTestConstants.JETSPEED_PROPERTIES_PATH));
                 
-        HashMap context = new HashMap();
-        OJBHelper ojbHelper = new OJBHelper(context);
-        ojbHelper.setUp();
         String appRoot = PortalTestConstants.JETSPEED_APPLICATION_ROOT;
         
         MockServletConfig servletConfig = new MockServletConfig();        
         ResourceLocatingServletContext servletContent = new ResourceLocatingServletContext(new File(appRoot));        
         servletConfig.setServletContext(servletContent);
         ServletConfigFactoryBean.setServletConfig(servletConfig);
-        ApplicationContext bootCtx = (ApplicationContext) context.get(AbstractTestHelper.APP_CONTEXT);
         
-        SpringComponentManager scm = new SpringComponentManager(new String[] {"../../etc/db-ojb/test-*.xml"}, new String[] {"/WEB-INF/assembly/*.xml"}, servletContent, appRoot );
+        SpringComponentManager scm = new SpringComponentManager(new String[] {"/WEB-INF/assembly/boot/datasource.xml"}, new String[] {"/WEB-INF/assembly/*.xml"}, servletContent, appRoot );
        
         engine = new JetspeedEngine(config, appRoot, servletConfig, scm );
         engine.start();
@@ -55,10 +52,7 @@ public class SpringEngineHelper extends OJBHelper
 
     public void tearDown() throws Exception
     {
-        engine.shutdown();  
-        super.tearDown();
+        engine.shutdown();
+        jndiDS.tearDown();
     }
-
-
-
 }
