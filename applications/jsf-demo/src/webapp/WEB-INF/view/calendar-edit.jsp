6dcdc2152914314567bff6b7847165916aad1c74
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2005-11-23 03:39:11 +0000

    http://issues.apache.org/jira/browse/JS2-413
    Simplify Build, step two
    moved out bridges apps
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@348352 13f79535-47bb-0310-9956-ffa450edef68

--- applications/jsf-demo/src/webapp/WEB-INF/view/calendar-edit.jsp
+++
@@ -1,1 +0,0 @@
-<h1>Calendar Edit Page</h1>
--- applications/php/src/webapp/hosts/conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$mysql_host = "localhost";		//mysql host (localhost if mysql is running on this machine)
-$mysql_user = "root";			//mysql user name
-$mysql_pwd = "";				//mysql password
-$mysql_db = "hosts";		//mysql database name
-
-?>
