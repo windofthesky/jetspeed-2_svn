929b612671e9eb32ea07547ebce8362e967a4b39
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2006-02-10 22:17:57 +0000

    tested and bug fixed transactional replay interceptors
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@376880 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/page-manager/src/test/org/apache/jetspeed/page/TestTransactions.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.page;
+
+import java.security.Principal;
+import java.security.PrivilegedAction;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+                                                                                                     
+import javax.security.auth.Subject;
+
+import org.apache.jetspeed.components.util.DatasourceEnabledSpringTestCase;
+import org.apache.jetspeed.om.common.SecurityConstraint;
+import org.apache.jetspeed.om.common.SecurityConstraints;
+import org.apache.jetspeed.om.folder.Folder;
+import org.apache.jetspeed.om.folder.FolderNotFoundException;
+import org.apache.jetspeed.om.page.Fragment;
+import org.apache.jetspeed.om.page.Link;
+import org.apache.jetspeed.om.page.Page;
+import org.apache.jetspeed.om.page.PageSecurity;
+import org.apache.jetspeed.om.page.SecurityConstraintsDef;
+import org.apache.jetspeed.security.UserPrincipal;
+import org.apache.jetspeed.security.impl.PrincipalsSet;
+import org.apache.jetspeed.security.impl.RolePrincipalImpl;
+import org.apache.jetspeed.security.impl.UserPrincipalImpl;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * TestSecureDatabasePageManager
+ * 
+ * @author <a href="rwatler@apache.org">Randy Watler</a>
+ * @version $Id: $
+ *          
+ */
+public class TestTransactions extends DatasourceEnabledSpringTestCase implements PageManagerTestShared
+{
+    protected PageManager pageManager;
+
+    protected String somePortletId;
+    
+    public static void main(String args[])
+    {
+        junit.awtui.TestRunner.main(new String[]
+        { TestTransactions.class.getName() });
+    }
+    
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        pageManager = (PageManager)ctx.getBean("pageManager");
+    }
+
+    public static Test suite()
+    {
+        // All methods starting with "test" will be executed in the test suite.
+        return new TestSuite(TestTransactions.class);
+    }
+    
+    protected String[] getConfigurations()
+    {
+        return new String[]
+        { "tx-page-manager.xml", "transaction.xml", "interceptors.xml" }; 
+    }
+
+    public void testTx() throws Exception
+    {
+        Folder root = pageManager.newFolder("/");
+        pageManager.updateFolder(root);
+        Page[] pages = new Page[3];
+        pages[0] = pageManager.newPage("/test1.psml");
+        pages[1] = pageManager.newPage("/test2.psml");
+        pages[2] = pageManager.newPage("/test3.psml");
+        try
+        {
+            pageManager.addPages(pages);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception adding pages" + e);
+            e.printStackTrace();
+            
+        }
+        assertFalse("page 1 found", pageManager.pageExists("/test1.psml"));
+        assertFalse("page 2 found", pageManager.pageExists("/test2.psml"));
+        assertFalse("page 3 found", pageManager.pageExists("/test3.psml"));
+    }
+}
