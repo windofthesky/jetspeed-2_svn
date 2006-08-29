c4bfb4f2358546d64aa7265778a9a06d78f7afb8
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2006-08-29 18:17:14 +0000

    JS2-574 permissions ajax action
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@438141 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/portal/src/java/org/apache/jetspeed/layout/impl/SecurityConstraintsAction.java
@@ -0,0 +1,310 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.layout.impl;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jetspeed.JetspeedActions;
+import org.apache.jetspeed.ajax.AJAXException;
+import org.apache.jetspeed.ajax.AjaxAction;
+import org.apache.jetspeed.ajax.AjaxBuilder;
+import org.apache.jetspeed.om.common.SecurityConstraint;
+import org.apache.jetspeed.om.page.PageSecurity;
+import org.apache.jetspeed.om.page.SecurityConstraintsDef;
+import org.apache.jetspeed.page.PageManager;
+import org.apache.jetspeed.request.RequestContext;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+/**
+ * Security Permission action
+ * 
+ * AJAX Parameters: 
+ *    action = constraints
+ *    method = add-def | update-def | remove-def | add-global | remove-global 
+ *    name = name of constraint definition or global definition
+ *    xml = the constraints payload, same format as PSML constraint defs
+ *    
+ * @author <a href="mailto:taylor@apache.org">David Sean Taylor </a>
+ * @version $Id: $
+ */
+public class SecurityConstraintsAction 
+    extends BasePortletAction 
+    implements AjaxAction, AjaxBuilder, Constants
+{
+    protected Log log = LogFactory.getLog(SecurityPermissionAction.class);
+    protected PageManager pm = null;
+
+    public SecurityConstraintsAction(String template, 
+                                     String errorTemplate, 
+                                     PageManager pm)
+    {
+        super(template, errorTemplate, null); 
+        this.pm = pm;
+    }
+    
+    public boolean run(RequestContext requestContext, Map resultMap)
+            throws AJAXException
+    {
+        boolean success = true;
+        String status = "success";
+        try
+        {
+            resultMap.put(ACTION, "constraints");
+            // Get the necessary parameters off of the request
+            String method = requestContext.getRequestParameter("method");
+            if (method == null) 
+            { 
+                throw new RuntimeException("Method not provided"); 
+            }            
+            resultMap.put("method", method);
+            if (false == checkAccess(requestContext, JetspeedActions.EDIT))
+            {
+                success = false;
+                resultMap.put(REASON, "Insufficient access to administer portal permissions");                
+                return success;
+            }           
+            int count = 0;
+            if (method.equals("add-def") || method.equals("update-def"))
+            {
+                count = updateConstraintDefinition(requestContext, resultMap);
+            }
+            else if (method.equals("remove-def"))
+            {
+                count = removeConstraintDefinition(requestContext, resultMap);
+            }
+            else if (method.equals("add-global"))
+            {
+                count = addGlobal(requestContext, resultMap);
+            }
+            else if (method.equals("remove-global"))
+            {
+                count = removeGlobal(requestContext, resultMap);
+            }
+            else
+            {
+                success = false;
+                resultMap.put(REASON, "Unsupported portal constraints method: " + method);                
+                return success;                
+            }
+            resultMap.put("count", Integer.toString(count));
+            resultMap.put(STATUS, status);
+        } 
+        catch (Exception e)
+        {
+            log.error("exception administering portal permissions", e);
+            resultMap.put(REASON, e.toString());
+            success = false;
+        }
+        return success;
+    }
+
+    protected int removeConstraintDefinition(RequestContext requestContext, Map resultMap)
+    throws AJAXException
+    {
+        String name = requestContext.getRequestParameter("name");
+        if (name == null)
+            throw new AJAXException("Missing 'name' parameter");
+        
+        try
+        {
+            PageSecurity pageSecurity = pm.getPageSecurity();        
+            SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(name);
+            if (def == null)
+            {
+                return 0;
+            }
+            List defs = pageSecurity.getSecurityConstraintsDefs();
+            defs.remove(def);
+            pageSecurity.setSecurityConstraintsDefs(defs);
+            pm.updatePageSecurity(pageSecurity);
+        }
+        catch (Exception e)
+        {
+            throw new AJAXException(e);
+        }        
+        return 1;
+    }
+    
+    protected int updateConstraintDefinition(RequestContext requestContext, Map resultMap)
+    throws AJAXException
+    {
+        int count = 0;
+        String xml = requestContext.getRequestParameter("xml");
+        if (xml == null)
+            throw new AJAXException("Missing 'xml' parameter");
+        try
+        {
+            SAXBuilder saxBuilder = new SAXBuilder();
+            StringReader reader = new StringReader(xml);
+            Document document = saxBuilder.build(reader);
+            Element root = document.getRootElement();
+            String name = root.getAttribute("name").getValue();
+            System.out.println("name = " + name);
+            PageSecurity pageSecurity = pm.getPageSecurity();
+            SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(name);
+            if (def == null)
+            {
+                def = pm.newSecurityConstraintsDef();                
+            }
+            int xmlSize = root.getChildren().size();
+            int defsSize = def.getSecurityConstraints().size();
+            int min = (xmlSize < defsSize) ? xmlSize : defsSize;
+            List xmlConstraints = root.getChildren();
+            List constraints = def.getSecurityConstraints();
+            for (int ix = 0; ix < min; ix++)
+            {
+                Element xmlConstraint = (Element)xmlConstraints.get(ix);
+                SecurityConstraint constraint =  (SecurityConstraint)constraints.get(ix);                
+                updateConstraintValues(xmlConstraint, constraint);
+                count++;                
+            }
+            if (xmlSize < defsSize)
+            {
+                // remove constraints
+                List deletes = new ArrayList(defsSize - xmlSize);
+                for (int ix = min; ix < defsSize; ix++)
+                {
+                    deletes.add(constraints.get(ix));
+                }
+                for (int ix = 0; ix < deletes.size(); ix++)
+                {
+                    constraints.remove(deletes.get(ix));
+                    count++;                    
+                }                
+            }
+            else if (xmlSize > defsSize)
+            {
+                // add new constraints
+                for (int ix = min; ix < xmlSize; ix++)
+                {
+                    Element xmlConstraint = (Element)xmlConstraints.get(ix);
+                    SecurityConstraint constraint =  pm.newPageSecuritySecurityConstraint();                    
+                    updateConstraintValues(xmlConstraint, constraint);
+                    constraints.add(constraint);
+                    count++;
+                }                
+            }
+            pm.updatePageSecurity(pageSecurity);
+        }
+        catch (Exception e)
+        {
+            throw new AJAXException(e);
+        }
+        return count;
+    }
+    
+    protected void updateConstraintValues(Element xmlConstraint, SecurityConstraint constraint)
+    {
+        constraint.setRoles(parseCSVList(xmlConstraint.getChildText("roles")));
+        constraint.setGroups(parseCSVList(xmlConstraint.getChildText("groups")));
+        constraint.setPermissions(parseCSVList(xmlConstraint.getChildText("permissions")));
+        constraint.setUsers(parseCSVList(xmlConstraint.getChildText("users")));        
+    }
+    
+    protected List parseCSVList(String csv)
+    {
+        if (csv != null)
+        {
+            List csvList = new ArrayList(4);
+            if (csv.indexOf(',') != -1)
+            {
+                StringTokenizer csvTokens = new StringTokenizer(csv, ",");
+                while (csvTokens.hasMoreTokens())
+                {
+                    csvList.add(csvTokens.nextToken().trim());
+                }
+            }
+            else
+            {
+                csvList.add(csv);
+            }
+            return csvList;
+        }
+        return null;
+    }
+    
+    protected int removeGlobal(RequestContext requestContext, Map resultMap)
+    throws AJAXException
+    {
+        int count = 0;
+        String name = requestContext.getRequestParameter("name");
+        if (name == null)
+            throw new AJAXException("Missing 'name' parameter");
+        
+        try
+        {
+            PageSecurity pageSecurity = pm.getPageSecurity();        
+            List globals = pageSecurity.getGlobalSecurityConstraintsRefs();
+            if (globals.contains(name))
+            {
+                return 0;
+            }
+            globals.remove(name);
+            pageSecurity.setGlobalSecurityConstraintsRefs(globals);
+            pm.updatePageSecurity(pageSecurity);
+            count++;
+        }
+        catch (Exception e)
+        {
+            throw new AJAXException(e);
+        }        
+        return count;
+    }
+       
+    protected int addGlobal(RequestContext requestContext, Map resultMap)
+    throws AJAXException
+    {
+        int count = 0;
+        boolean found = false;
+        String name = requestContext.getRequestParameter("name");
+        if (name == null)
+            throw new AJAXException("Missing 'name' parameter");
+        
+        try
+        {
+            PageSecurity pageSecurity = pm.getPageSecurity();        
+            List globals = pageSecurity.getGlobalSecurityConstraintsRefs();
+            ;
+            if (pageSecurity.getSecurityConstraintsDef(name) == null)
+            {
+                throw new AJAXException("global name doesnt exist in definitions");
+            }
+            if (globals.contains(name))
+            {
+                // already exist;
+                return count;
+            }
+            globals.add(name);
+            pageSecurity.setGlobalSecurityConstraintsRefs(globals);
+            pm.updatePageSecurity(pageSecurity);
+            count++;
+        }
+        catch (Exception e)
+        {
+            throw new AJAXException(e);
+        }        
+        return count;
+    }
+    
+}
---
+++ components/portal/src/test/org/apache/jetspeed/layout/TestConstraintsAction.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright 2000-2001,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.layout;
+
+import java.io.File;
+import java.security.PrivilegedAction;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+
+import junit.framework.TestCase;
+
+import org.apache.jetspeed.ajax.AjaxRequestService;
+import org.apache.jetspeed.components.ComponentManager;
+import org.apache.jetspeed.components.SpringComponentManager;
+import org.apache.jetspeed.components.factorybeans.ServletConfigFactoryBean;
+import org.apache.jetspeed.layout.impl.LayoutValve;
+import org.apache.jetspeed.mocks.ResourceLocatingServletContext;
+import org.apache.jetspeed.om.common.SecurityConstraint;
+import org.apache.jetspeed.om.page.PageSecurity;
+import org.apache.jetspeed.om.page.SecurityConstraintsDef;
+import org.apache.jetspeed.page.PageManager;
+import org.apache.jetspeed.pipeline.PipelineException;
+import org.apache.jetspeed.request.JetspeedRequestContext;
+import org.apache.jetspeed.request.RequestContext;
+import org.apache.jetspeed.security.impl.RolePrincipalImpl;
+import org.apache.jetspeed.security.impl.UserPrincipalImpl;
+import org.apache.velocity.app.VelocityEngine;
+
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+import com.mockrunner.mock.web.MockHttpSession;
+import com.mockrunner.mock.web.MockServletConfig;
+import com.mockrunner.mock.web.MockServletContext;
+
+/**
+ * Test Security Constraints Manipulation
+ *  
+ * @author <a>David Sean Taylor </a>
+ * @version $Id: $
+ */
+public class TestConstraintsAction extends TestCase
+{
+
+    private ComponentManager cm;
+
+    private LayoutValve valve;
+    
+    private VelocityEngine velocity;
+    
+    private AjaxRequestService ajax;
+    
+    private PageManager pageManager;
+
+    public static void main(String[] args)
+    {
+        junit.swingui.TestRunner.run(TestLayout.class);
+    }
+
+    /**
+     * Setup the request context
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        String appRoot =  "./"; //PortalTestConstants.JETSPEED_APPLICATION_ROOT;
+        
+        MockServletConfig servletConfig = new MockServletConfig();        
+        ResourceLocatingServletContext servletContent = new ResourceLocatingServletContext(new File(appRoot));        
+        servletConfig.setServletContext(servletContent);
+        ServletConfigFactoryBean.setServletConfig(servletConfig);
+        
+        // Load the Spring configs
+        String[] bootConfigs = null;
+        String[] appConfigs =
+        { //"src/webapp/WEB-INF/assembly/layout-api.xml",
+                "src/test/resources/assembly/test-layout-constraints-api.xml",
+                "src/test/resources/assembly/page-manager.xml"};
+        
+                
+        cm = new SpringComponentManager(bootConfigs, appConfigs, servletContent, ".");
+        cm.start();
+        valve = (LayoutValve) cm.getComponent("layoutValve");
+        velocity = (VelocityEngine) cm.getComponent("AjaxVelocityEngine");
+        ajax = (AjaxRequestService) cm.getComponent("AjaxRequestService");
+        pageManager = (PageManager) cm.getComponent("pageManager");
+    }
+
+    protected void tearDown() throws Exception
+    {
+        cm.stop();
+    }
+
+    public void testUpdate()
+    throws Exception
+    {
+        String method = "update-def";
+        String defName = "users";
+        String xml =
+            "<security-constraints-def name=\"" + 
+                  defName + 
+                  "\"><security-constraint><roles>user, manager</roles><permissions>view,edit</permissions></security-constraint></security-constraints-def>";
+        runTest(xml, defName, method);
+        PageSecurity pageSecurity = pageManager.getPageSecurity();
+        SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(defName);
+        assertNotNull("definition " + defName + " not found ", def);
+        SecurityConstraint constraint =  (SecurityConstraint)def.getSecurityConstraints().get(0);
+        assertNotNull("first constraint for " + defName + " not found ", def);
+        assertEquals("update failed for constraints " + constraint.getPermissions().toString(), constraint.getPermissions().toString(), "[view, edit]");
+    }
+
+    public void testAdds()
+    throws Exception
+    {
+        String method = "update-def";        
+        String defName = "users";
+        String xml =
+            "<security-constraints-def name=\"" + 
+                  defName + 
+                  "\"><security-constraint><roles>user, manager,anon</roles><permissions>view,edit,help</permissions></security-constraint>" +
+                  "<security-constraint><groups>accounting,finance</groups><permissions>view,edit,help</permissions></security-constraint>" +
+                  "<security-constraint><users>tomcat</users><permissions>view</permissions></security-constraint>" +
+                  "<security-constraint><users>manager,admin</users><permissions>view,help</permissions></security-constraint>" +
+                  "</security-constraints-def>";
+                  
+        runTest(xml, defName, method);
+        PageSecurity pageSecurity = pageManager.getPageSecurity();
+        SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(defName);
+        assertNotNull("definition " + defName + " not found ", def);
+        SecurityConstraint constraint =  (SecurityConstraint)def.getSecurityConstraints().get(0);
+        assertNotNull("first constraint for " + defName + " not found ", constraint);
+        assertEquals("update failed for constraints " + constraint.getPermissions().toString(), constraint.getPermissions().toString(), "[view, edit, help]");
+        assertEquals("update failed for constraints " + constraint.getRoles().toString(), constraint.getRoles().toString(), "[user, manager, anon]");
+        
+        SecurityConstraint constraint2 =  (SecurityConstraint)def.getSecurityConstraints().get(1);
+        assertNotNull("second constraint for " + defName + " not found ", constraint2);
+        assertEquals("add failed for constraints " + constraint2.getPermissions().toString(), constraint2.getPermissions().toString(), "[view, edit, help]");
+        assertEquals("add failed for constraints " + constraint2.getGroups().toString(), constraint2.getGroups().toString(), "[accounting, finance]");
+
+        SecurityConstraint constraint3 =  (SecurityConstraint)def.getSecurityConstraints().get(2);
+        assertNotNull("third constraint for " + defName + " not found ", constraint3);
+        assertEquals("add failed for constraints " + constraint3.getPermissions().toString(), constraint3.getPermissions().toString(), "[view]");
+        assertEquals("add failed for constraints " + constraint3.getUsers().toString(), constraint3.getUsers().toString(), "[tomcat]");
+
+        SecurityConstraint constraint4 =  (SecurityConstraint)def.getSecurityConstraints().get(3);
+        assertNotNull("fourth constraint for " + defName + " not found ", constraint4);
+        assertEquals("add failed for constraints " + constraint4.getPermissions().toString(), constraint4.getPermissions().toString(), "[view, help]");
+        assertEquals("add failed for constraints " + constraint4.getUsers().toString(), constraint4.getUsers().toString(), "[manager, admin]");
+        
+    }
+
+    public void testDeletes()
+    throws Exception
+    {
+        String method = "update-def";        
+        String defName = "delete3";
+        String xml =
+            "<security-constraints-def name=\"" + 
+                  defName + 
+                  "\"><security-constraint><users>*</users><permissions>view</permissions></security-constraint></security-constraints-def>";
+        runTest(xml, defName, method);
+        PageSecurity pageSecurity = pageManager.getPageSecurity();
+        SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(defName);
+        assertNotNull("definition " + defName + " not found ", def);
+        SecurityConstraint constraint =  (SecurityConstraint)def.getSecurityConstraints().get(0);
+        assertNotNull("first constraint for " + defName + " not found ", def);
+        assertEquals("delete merge failed for constraints " + constraint.getPermissions().toString(), constraint.getPermissions().toString(), "[view]");
+        assertEquals("delete merge failed for constraints " + constraint.getUsers().toString(), constraint.getUsers().toString(), "[*]");        
+        assertTrue("constrainst size should be 1 ", def.getSecurityConstraints().size() == 1);        
+    }
+
+    public void testDeleteDef()
+    throws Exception
+    {
+        String method = "remove-def";        
+        String defName = "deleteme";
+        String xml = "";
+        runTest(xml, defName, method);
+        PageSecurity pageSecurity = pageManager.getPageSecurity();
+        SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(defName);
+        assertNull("definition " + defName + " should be deleted ", def);
+    }
+
+    public void testAddGlobal()
+    throws Exception
+    {
+        String method = "add-global";        
+        String defName = "manager";
+        String xml = "";
+        runTest(xml, defName, method);
+        PageSecurity pageSecurity = pageManager.getPageSecurity();
+        SecurityConstraintsDef def = pageSecurity.getSecurityConstraintsDef(defName);
+        List globals = pageSecurity.getGlobalSecurityConstraintsRefs();
+        assertTrue("should have found new global " + defName,  globals.contains(defName));
+        assertTrue("should have found old global " + defName,  globals.contains("admin"));
+    }
+    
+    public void runTest(String xml, String defName, String method)
+    throws Exception
+    {
+        MockServletConfig config = new MockServletConfig();
+        MockServletContext context = new MockServletContext();
+        MockHttpSession session = new MockHttpSession();
+        session.setupServletContext(context);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setupAddParameter("action", "constraints");
+        request.setupAddParameter("method", method);
+        request.setupAddParameter("xml", xml);
+        request.setupAddParameter("name", defName);
+        request.setSession(session);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        final RequestContext rc = 
+            new JetspeedRequestContext(request, response, config, null);
+        
+        Set principals = new HashSet();
+        principals.add(new UserPrincipalImpl("admin"));
+        principals.add(new RolePrincipalImpl("admin"));
+        Subject subject = new Subject(true, principals, new HashSet(), new HashSet());
+        
+        PipelineException pe = (PipelineException) Subject.doAsPrivileged(subject, new PrivilegedAction()
+                {
+                    public Object run() 
+                    {
+                         try
+                        {
+                             valve.invoke(rc, null);                 
+                            return null;
+                        }
+                        catch (PipelineException e)
+                        {
+                            return e;
+                        }                    
+                    }
+                }, null);
+     
+        
+    }
+    
+
+}
