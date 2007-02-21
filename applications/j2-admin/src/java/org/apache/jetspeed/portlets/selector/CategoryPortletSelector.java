9547a09a19aafd3f9c3e897a2ab5e61c72685c74
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2007-02-21 22:56:44 +0000

    rolling back again to proper usage of portlet api (i think i may have screwed up revision 509931, as I seem to have accidently written over it with 50997)
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@510286 13f79535-47bb-0310-9956-ffa450edef68

--- applications/j2-admin/src/java/org/apache/jetspeed/portlets/selector/CategoryPortletSelector.java
+++ applications/j2-admin/src/java/org/apache/jetspeed/portlets/selector/CategoryPortletSelector.java
@@ -522,6 +522,18 @@ public class CategoryPortletSelector extends AbstractDojoVelocityPortlet impleme
                 session.removeAttribute(PORTLETS);
                 session.removeAttribute(CATEGORIES);
             }
+            String category = request.getParameter(CATEGORY);
+            if (category != null)
+                actionResponse.setRenderParameter(CATEGORY, category);
+            String pageNumber = request.getParameter(PAGENUMNER);
+            if (pageNumber != null)
+                actionResponse.setRenderParameter(PAGENUMNER, pageNumber);                
+            String filter = request.getParameter(FILTER);
+            if (filter != null)
+                actionResponse.setRenderParameter(FILTER, filter);
+            String pagePath = request.getParameter(JSPAGE);
+            if (pagePath != null)
+                actionResponse.setRenderParameter(JSPAGE, pagePath);            
         }
     }
 
