41fb5898a5b88c4d568e6aac402087a9cacfff2a
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2007-10-24 05:05:51 +0000

    make sendEmail public api
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@587790 13f79535-47bb-0310-9956-ffa450edef68

--- jetspeed-api/src/main/java/org/apache/jetspeed/administration/PortalAdministration.java
+++ jetspeed-api/src/main/java/org/apache/jetspeed/administration/PortalAdministration.java
@@ -22,6 +22,8 @@ import java.util.Map;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
+
+import org.apache.jetspeed.administration.AdministrationEmailException;
 import org.apache.jetspeed.security.User;
 
 /**
@@ -104,6 +106,17 @@ public interface PortalAdministration
                           String templatePath,
                           Map userAttributes)
         throws AdministrationEmailException;
+    
+    /**
+     * Helper to send an email to a recipient without the portal default sender, and without mail merge
+     * 
+     * @param from the email address of the sender
+     * @param subject the subject of the email
+     * @param to the recipient email address
+     * @param text the message text
+     * @throws AdministrationEmailException
+     */
+    public void sendEmail(String from, String subject, String to, String text) throws AdministrationEmailException;    
     
     /**
      * Lookup a user given an email address
