c61c060664ff72e32f34b43516d7f66d5aa3a038
smilek <smilek@13f79535-47bb-0310-9956-ffa450edef68>
2007-12-08 09:33:09 +0000

    The following revision comments are from branches/JETSPEED-2.1.3 revisions which were not individually checked-in to trunk === ---------------------------------------------------------------- === [ from branches/JETSPEED-2.1.3 r595411 ] | smilek | 2007-11-15 13:06:54 -0700 (Thu, 15 Nov 2007) | 1 line
    major improvements to page editor (UI, config options, flow, no action layouts); support for create-new-page-on-edit
    === ---------------------------------------------------------------- ===
    [ from branches/JETSPEED-2.1.3 r583998 ] | smilek | 2007-10-11 18:36:12 -0600 (Thu, 11 Oct 2007) | 1 line
    
    continuation of desktop overhaul
    === ---------------------------------------------------------------- ===
    [ from branches/JETSPEED-2.1.3 r578909 ] | smilek | 2007-09-24 13:08:29 -0600 (Mon, 24 Sep 2007) | 1 line
    
    desktop overhaul - PortletWindow is no longer a dojo widget
    === ---------------------------------------------------------------- ===
    
    
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/trunk@602419 13f79535-47bb-0310-9956-ffa450edef68

--- jetspeed-portal-resources/src/main/resources/webapp/javascript/jetspeed/widget/PageEditPane.js
+++ jetspeed-portal-resources/src/main/resources/webapp/javascript/jetspeed/widget/PageEditPane.js
@@ -42,71 +42,103 @@ this.createPageDialog.destroy();
 }
 jetspeed.widget.PageEditPane.superclass.destroy.apply(this,arguments);
 },postCreate:function(_b,_c,_d){
-jetspeed.widget.PageEditPane.superclass.postCreate.apply(this,arguments);
-if(!dojo.render.html.ie){
+var _e=jetspeed;
+var _f=dojo.html;
+_e.widget.PageEditPane.superclass.postCreate.apply(this,arguments);
+var _10=_e.widget.PageEditor.prototype;
 if(this.pageEditContainer!=null){
-this.pageEditContainer.style.backgroundColor="#d3d3d3";
+_f.addClass(this.pageEditContainer,_10.styleBaseAdd);
 }
 if(this.pageEditLDContainer!=null){
-this.pageEditLDContainer.style.backgroundColor="#eeeeee";
+_f.addClass(this.pageEditLDContainer,_10.styleDetailAdd);
 }
 if(this.pageEditPDContainer!=null){
-this.pageEditPDContainer.style.backgroundColor="#eeeeee";
-}
+_f.addClass(this.pageEditPDContainer,_10.styleDetailAdd);
 }
 if(this.layoutDecoratorSelect!=null){
-var _e=jetspeed.page.layoutDecorator;
-var _f=[];
+if(!_10.checkPerm(_10.PM_PG_L_D,_e)){
+if(this.pageEditLDContainer){
+this.pageEditLDContainer.style.display="none";
+}else{
+this.layoutDecoratorSelect.domNode.style.display="none";
+}
+}else{
+var _11=_e.page.layoutDecorator;
+var _12=[];
 if(this.layoutDecoratorDefinitions){
 for(var i=0;i<this.layoutDecoratorDefinitions.length;i++){
-var _11=this.layoutDecoratorDefinitions[i];
-if(_11&&_11.length==2){
-_f.push([_11[0],_11[1]]);
-if(_e==_11[1]){
-this.layoutDecoratorSelect.setAllValues(_11[0],_11[1]);
+var _14=this.layoutDecoratorDefinitions[i];
+if(_14&&_14.length==2){
+_12.push([_14[0],_14[1]]);
+if(_11==_14[1]){
+this.layoutDecoratorSelect.setAllValues(_14[0],_14[1]);
 }
 }
 }
 }
-this.layoutDecoratorSelect.dataProvider.setData(_f);
+this.layoutDecoratorSelect.dataProvider.setData(_12);
+}
 }
 if(this.portletDecoratorSelect!=null){
-var _12=jetspeed.page.portletDecorator;
-var _13=[];
+if(!_10.checkPerm(_10.PM_PG_P_D,_e)){
+if(this.pageEditPDContainer){
+this.pageEditPDContainer.style.display="none";
+}else{
+this.portletDecoratorSelect.domNode.style.display="none";
+}
+}else{
+var _15=_e.page.portletDecorator;
+var _16=[];
 if(this.portletDecoratorDefinitions){
 for(var i=0;i<this.portletDecoratorDefinitions.length;i++){
-var _14=this.portletDecoratorDefinitions[i];
-if(_14&&_14.length==2){
-_13.push([_14[0],_14[1]]);
-if(_12==_14[1]){
-this.portletDecoratorSelect.setAllValues(_14[0],_14[1]);
+var _17=this.portletDecoratorDefinitions[i];
+if(_17&&_17.length==2){
+_16.push([_17[0],_17[1]]);
+if(_15==_17[1]){
+this.portletDecoratorSelect.setAllValues(_17[0],_17[1]);
 }
 }
 }
 }
-this.portletDecoratorSelect.dataProvider.setData(_13);
+this.portletDecoratorSelect.dataProvider.setData(_16);
+}
+}
+var _18=_10.checkPerm(_10.PM_PG_AD,_e);
+if(!_18){
+this.createPageButton.domNode.style.display="none";
 }
 },deletePage:function(){
-this.deletePageDialog.show();
+this.pageEditorWidget._openDialog(this.deletePageDialog);
 },deletePageConfirmed:function(){
-var _15=new jetspeed.widget.RemovePageContentManager(this.pageEditorWidget);
-_15.getContent();
+var _19=new jetspeed.widget.RemovePageContentManager(this.pageEditorWidget);
+_19.getContent();
 },createPage:function(){
-this.createPageDialog.show();
-},createPageConfirmed:function(_16,_17,_18){
-if(_16!=null&&_16.length>0){
-var _19=jetspeed.page.getPageDirectory(true)+_16;
-var _1a=jetspeed.page.getPageDirectory()+_16;
-var _1b=new jetspeed.widget.AddPageContentManager(_19,_1a,_16,null,_17,_18,this.pageEditorWidget);
-_1b.getContent();
+this.pageEditorWidget._openDialog(this.createPageDialog);
+},createPageConfirmed:function(_1a,_1b,_1c){
+if(_1a!=null&&_1a.length>0){
+var _1d=jetspeed.page.getPageDirectory(true)+_1a;
+var _1e=jetspeed.page.getPageDirectory()+_1a;
+var _1f=new jetspeed.widget.AddPageContentManager(_1d,_1e,_1a,null,_1b,_1c,this.pageEditorWidget);
+_1f.getContent();
 }
 },changeLayoutDecorator:function(){
-var _1c=new jetspeed.widget.UpdatePageInfoContentManager(this.layoutDecoratorSelect.getValue(),null,this.pageEditorWidget);
-_1c.getContent();
+var _20=new jetspeed.widget.UpdatePageInfoContentManager(this.layoutDecoratorSelect.getValue(),null,this.pageEditorWidget);
+_20.getContent();
 },changePortletDecorator:function(){
-var _1d=new jetspeed.widget.UpdatePageInfoContentManager(null,this.portletDecoratorSelect.getValue(),this.pageEditorWidget);
-_1d.getContent();
+var _21=new jetspeed.widget.UpdatePageInfoContentManager(null,this.portletDecoratorSelect.getValue(),this.pageEditorWidget);
+_21.getContent();
 },editModeRedisplay:function(){
 this.show();
+},onBrowserWindowResize:function(){
+var _22=this.deletePageDialog;
+var _23=this.createPageDialog;
+if(_22&&_22.isShowing()){
+_22.domNode.style.display="none";
+_22.domNode.style.display="block";
+}
+if(_23&&_23.isShowing()){
+_23.domNode.style.display="none";
+_23.domNode.style.display="block";
+}
 }});
 
