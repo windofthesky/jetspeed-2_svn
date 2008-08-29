06d50aadbd92461fd659a1042752f714ef025a81
vkumar <vkumar@13f79535-47bb-0310-9956-ffa450edef68>
2008-08-29 18:39:17 +0000

    Refactoring of the security model and Api
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/portal/branches/security-refactoring@690363 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jetspeed-api/src/main/java/org/apache/jetspeed/security/JetspeedPrincipalAccessManager.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.security;
+
+import java.util.List;
+
+public interface JetspeedPrincipalAccessManager {
+    boolean principalExists(String principalName, JetspeedPrincipalType type);
+    JetspeedPrincipal getPrincipal(Long id);
+    JetspeedPrincipal getPrincipal(String principalName, JetspeedPrincipalType type);
+    List<JetspeedPrincipal> getPrincipals(String nameFilter, List<SecurityAttributes> attributes, JetspeedPrincipalType type);
+    List<String> getPrincipalNames(String nameFilter, List<SecurityAttributes> attributes, JetspeedPrincipalType type);
+    List<JetspeedPrincipal> getAssociatedFrom(String principalName, JetspeedPrincipalType type, String associationName);
+    List<JetspeedPrincipal> getAssociatedFrom(Long principalId, JetspeedPrincipalType type, String associationName);
+    List<JetspeedPrincipal> getAssociatedTo(String principalName, JetspeedPrincipalType type, String associationName);
+    List<JetspeedPrincipal> getAssociatedTo(Long principalId, JetspeedPrincipalType type, String associationName);
+    List<String> getAssociatedNamesFrom(String principalName, JetspeedPrincipalType type, String associationName);
+    List<String> getAssociatedNamesFrom(Long principalId, JetspeedPrincipalType type, String associationName);
+    List<String> getAssociatedNamesTo(String principalName, JetspeedPrincipalType type, String associationName);
+    List<String> getAssociatedNamesTo(Long principalId, JetspeedPrincipalType type, String associationName);
+    List<String> getPrincipals(JetspeedPermission permission);
+    List<String> getPrincipals(JetspeedPermission permission, JetspeedPrincipalType type);
+}
---
+++ jetspeed-api/src/main/java/org/apache/jetspeed/security/JetspeedPrincipalManager.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jetspeed.security;
+
+import java.util.List;
+
+
+public interface JetspeedPrincipalManager {
+    JetspeedPrincipalType getPrincipalType();
+    void setAccessManager(JetspeedPrincipalAccessManager pm);
+    void setStorageManager(JetspeedPrincipalStorageManager sm);
+    void setPermissionStorageManager(JetspeedPrincipalPermissionStorageManager sm);
+    JetspeedCredentialManager getCredentialManager();
+    void addAssociationHandler(JetspeedPrincipalAssociationHandler ah);
+    JetspeedPrincipal newPrincipal(String name);
+    boolean principalExists(String name);
+    JetspeedPrincipal getPrincipal(String name);
+    List<String> getPrincipalNames(String nameFilter, List<SecurityAttributes> attributes);
+    List<JetspeedPrincipal> getPrincipals(String nameFilter, List<SecurityAttributes> attributes);
+    void removePrincipal(String name) throws PrincipalNotFoundException, PrincipalNotRemovableException, DependentPrincipalException;
+    List <JetspeedPrincipal> getAssociatedFrom(String principalName, String associationName);
+    List <JetspeedPrincipal> getAssociatedTo(String principalName, String associationName);
+    List <String> getAssociatedNamesFrom(String principalName, String associationName);
+    List <String> getAssociatedNamesTo(String principalName, String associationName);
+}
