65efb1216483e3d4fd47866f061ab66f7ae6d18b
ate <ate@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-11 22:04:45 +0000

    Moving the PortalResourcePermission and the extended classes under the security component security.spi.impl package. For a short while this breaks the existing usages but I'll be providing a factory solution through the new JetspeedPermissionManager  shortly
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/portal/branches/security-refactoring@694509 13f79535-47bb-0310-9956-ffa450edef68

--- components/jetspeed-security/src/main/java/org/apache/jetspeed/security/spi/impl/PagePermission.java
+++ components/jetspeed-security/src/main/java/org/apache/jetspeed/security/spi/impl/PagePermission.java
@@ -14,7 +14,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.jetspeed.security;
+package org.apache.jetspeed.security.spi.impl;
 
 import java.security.Permission;
 
