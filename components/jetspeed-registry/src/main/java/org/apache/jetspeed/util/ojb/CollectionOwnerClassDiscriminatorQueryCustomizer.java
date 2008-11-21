a3cf258f699941d10cd7707ad87581df7d046cdd
ate <ate@13f79535-47bb-0310-9956-ffa450edef68>
2008-11-21 15:57:09 +0000

    Fixing Description/DisplayName retrieval based on containing object. This needs "discriminator" query filtering from the owner object side which I forgot to implement. Now handled properly using a custom OJB QueryCustomizer
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/portal/branches/JS2-871-pluto-2.0-upgrade@719611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/jetspeed-registry/src/main/java/org/apache/jetspeed/util/ojb/CollectionOwnerClassDiscriminatorQueryCustomizer.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jetspeed.util.ojb;
+
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.accesslayer.QueryCustomizerDefaultImpl;
+import org.apache.ojb.broker.metadata.CollectionDescriptor;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryByCriteria;
+
+/**
+ * @version $Id$
+ *
+ */
+public class CollectionOwnerClassDiscriminatorQueryCustomizer extends QueryCustomizerDefaultImpl
+{
+    private static final long serialVersionUID = -1933868940330442307L;
+    
+    private static final String DISCRIMINATOR_FIELD = "discriminatorField"; 
+
+    public Query customizeQuery(Object owner, PersistenceBroker broker, CollectionDescriptor collDescriptor, QueryByCriteria query)
+    {
+        String discriminatorField = this.getAttribute(DISCRIMINATOR_FIELD);
+        if (discriminatorField != null)
+        {
+            query.getCriteria().addEqualTo(discriminatorField, owner.getClass().getName());
+        }
+        return query;
+    }
+}
