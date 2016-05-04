e49771a392ab8994f41905beb4ba87abb9f434b2
taylor <taylor@13f79535-47bb-0310-9956-ffa450edef68>
2016-05-04 03:29:10 +0000

    JS2-1355: release preparation. bumping tomcat to latest version for installer. adding release procedure docs
    git-svn-id: http://svn.apache.org/repos/asf/portals/jetspeed-2/portal/trunk@1742214 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pre-release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g applications/jetspeed-demo/jetspeed-mvn-demo-pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g enterprise/ear-full/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g enterprise/ear-min/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g enterprise/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-archetype/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-archetype/src/main/resources/META-INF/maven/archetype-metadata.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-archetype/src/main/resources/archetype-resources/__rootArtifactId__-portal/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-archetype/src/main/resources/archetype-resources/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/BUILD.txt
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/etc/ant-installer/antinstall-config.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/etc/ant-installer/build.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/etc/database/database.properties.template
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/jetspeed-mvn-ant-installer-pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/jetspeed-mvn-database-pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/jetspeed-mvn-tomcat-portal-pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-installer/pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-mvn-db-init-pom.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g src/site/site.xml
+sed -i.bak s/2.3.1-SNAPSHOT/2.3.1/g jetspeed-portal-resources/src/main/resources/conf/jetspeed/jetspeed.properties
+find . -name "*.bak" -type f -delete
